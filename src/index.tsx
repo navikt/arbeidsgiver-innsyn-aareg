6590ef69e55bb5f00245d98f8ce851c8021bd79e
Hilde Steinbru Heggstad <h155771@Hildes-MacBook-Pro.local>
2019-11-19 15:51:45 +0100

    konfigurerer

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import 'core-js';
+import 'unorm/lib/unorm';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.less';
+import App from './App/App';
+
+if (process.env.REACT_APP_MOCK) {
+    console.log('========================================');
+    console.log('=============== MED MOCK ===============');
+    console.log('===DETTE SKAL DU IKKE SE I PRODUKSJON===');
+    console.log('========================================');
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
