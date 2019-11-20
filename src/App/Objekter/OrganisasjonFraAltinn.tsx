5088e8c92f15937db90b4ab33c39a1c25b22ffd1
Hilde Steinbru Heggstad <h155771@Hildes-MacBook-Pro.local>
2019-11-20 12:05:51 +0100

    lagd inngangsside med kall til AAreg

---
+++ src/App/Objekter/OrganisasjonFraAltinn.tsx
@@ -0,0 +1,8 @@
+export interface OrganisasjonFraAltinn {
+    Name: string;
+    Type: string;
+    OrganizationNumber: string;
+    OrganizationForm: string;
+    Status: string;
+    ParentOrganizationNumber: string;
+}
