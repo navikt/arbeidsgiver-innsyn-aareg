b677b083038c9c86b08bf30739fdd7271d82db48
Hilde Steinbru Heggstad <h155771@Hildes-MacBook-Pro.local>
2019-11-20 13:18:14 +0100

    fatt inn logikk pa bygge orgtre til banner

--- src/App/api/AAregApi.tsx
+++ src/App/api/AAregApi.tsx
@@ -1,4 +1,6 @@
-import {hentArbeidsforholdLink} from "../lenker";
+import {hentArbeidsforholdLink, hentOverordnetEnhetApiLink, hentUnderenhetApiLink} from "../lenker";
+import {ListeMedJuridiskeEnheter, OrganisasjonFraEnhetsregisteret} from "../Objekter/OrganisasjonFraEnhetsregisteret";
+import {OrganisasjonFraAltinn, tomaAltinnOrganisasjon} from "../Objekter/OrganisasjonFraAltinn";
 
 export async function hentArbeidsforhold(orgnr: string): Promise<any> {
     let respons = await fetch(hentArbeidsforholdLink(orgnr));
@@ -7,4 +9,58 @@ export async function hentArbeidsforhold(orgnr: string): Promise<any> {
     } else {
         return null;
     }
+}
+
+export async function hentUnderenhet(orgnr: string): Promise<any> {
+    let respons = await fetch(hentUnderenhetApiLink(orgnr));
+    if (respons.ok) {
+        const enhet: OrganisasjonFraEnhetsregisteret = await respons.json();
+        return enhet;
+    }
+    return null;
+}
+
+export async function hentOverordnetEnhet(orgnr: string): Promise<any> {
+    if (orgnr !== '') {
+        let respons = await fetch(hentOverordnetEnhetApiLink(orgnr));
+        if (respons.ok) {
+            const enhet: OrganisasjonFraEnhetsregisteret = await respons.json();
+            return enhet;
+        }
+    }
+    return null;
+}
+
+export async function hentAlleJuridiskeEnheter(
+    listeMedJuridiskeOrgNr: string[]
+): Promise<any> {
+    let url: string = 'https://data.brreg.no/enhetsregisteret/api/enheter/?organisasjonsnummer=';
+    const distinkteJuridiskeEnhetsnr: string[] = listeMedJuridiskeOrgNr.filter(
+        (jurOrg, index) => listeMedJuridiskeOrgNr.indexOf(jurOrg) === index
+    );
+    distinkteJuridiskeEnhetsnr.forEach(orgnr => {
+        if (distinkteJuridiskeEnhetsnr.indexOf(orgnr) === 0) {
+            url += orgnr;
+        } else {
+            url += ',' + orgnr;
+        }
+    });
+    let respons = await fetch(url);
+    if (respons.ok) {
+        const distinkteJuridiskeEnheterFraEreg: ListeMedJuridiskeEnheter = await respons.json();
+        let distinkteJuridiskeEnheter: OrganisasjonFraAltinn[] = distinkteJuridiskeEnheterFraEreg._embedded.enheter.map(
+            orgFraEereg => {
+
+                const jurOrg: OrganisasjonFraAltinn = {
+                    ...tomaAltinnOrganisasjon,
+                    Name: orgFraEereg.navn,
+                    OrganizationNumber: orgFraEereg.organisasjonsnummer,
+                };
+                return jurOrg;
+            }
+        );
+        return distinkteJuridiskeEnheter;
+    }
+
+    return [];
 }
