d4d5c3bfdb66744976c96e050f18ad03d236f96e
Hilde Steinbru Heggstad <h155771@Hildes-MacBook-Pro.local>
2019-11-20 09:55:10 +0100

    initiert react app

--- craco.config.js
+++
@@ -1,3 +0,0 @@
-const CracoLessPlugin = require("craco-less");
-
-
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,12 @@
-import 'core-js';
-import 'unorm/lib/unorm';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.less';
-import App from './App/App';
-
-if (process.env.REACT_APP_MOCK) {
-    console.log('========================================');
-    console.log('=============== MED MOCK ===============');
-    console.log('===DETTE SKAL DU IKKE SE I PRODUKSJON===');
-    console.log('========================================');
-}
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
