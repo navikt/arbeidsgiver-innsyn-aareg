a0968dce243d02773ec224ecc172d6c7be74a379
hilde.heggstad <hilde.heggstad@bekk.no>
2019-11-21 12:35:06 +0100

    lagd liste med attributter for å generere mockingdata

---
+++ src/mocking/funksjonerForÅgenerereMockData.tsx
@@ -0,0 +1,12 @@
+const listeMedFornavn: string[]= ["Gudrun", "Elina", "Kaia", "Knut", "Jenny", "Petter", "Martin", "Marie", "Herman", "Alfred", "Leif", "Inger", "Ivar", "Trond"];
+
+const listeMedEtterNavn: string[] = ["Rishovd", "Knuten", "Ludvigsen", "Solberg", "Stoltenberg", "Støre", "Ibsen", "Munch", "Vang", "Nesbø", "Morgenstierne"];
+
+const datoFortid: string[] = ["29/01/1996", "01/04/1999", "01/12/1998", "18/04/1990", "14/02/1990", "01/05/1980", "17/05/2000", "17/05/1814"];
+
+const datoFramtid: string[] = ["29/01/2020", "01/04/2021", "01/12/2024", "18/04/2020", "14/02/2021", "01/05/2025", "17/05/2020", "17/05/2020"];
+
+const yrker: string[] = ["Ekspeditør", "Prsonalansvarlig", "Daglig leder", "Servitør", "Pianist", "Lektor", "Gymlærer", "Konsulent", "Produkteier", "Generalsekretær", "Arkitekt", "Slangetemmer", "Performer", "Torpedo"];
+
+const fodselsNr: string []= ["04015226825","15119702590","30067234940","22059007517","14039717019","08020285185","20106012971","20085624661","08024706711","26075014618","14117227856","03045013986","08114503186","19105737176","05037702090","14077541803","07106728814","28079345867","28077403517","23097011680","14109431703","03049219872","09037947773","21038137589","17123920384","06047414707","21123832989"];
+
