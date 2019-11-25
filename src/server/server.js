b244ceae94fd0497f3a38f4bdf5698a6e7834ee6
bendiksi <benibe88@gmail.com>
2019-11-25 21:37:07 +0100

    korrigert path til build mappa i server

--- src/server/server.js
+++ src/server/server.js
@@ -1,16 +1,16 @@
 const path = require('path');
 const express = require('express');
-const BASE_PATH='/bedriftsoversikt-og-ansatte';
+const BASE_PATH='/bedriftsoversikt-og-ansatte/';
 const server = express();
+server.use(BASE_PATH, express.static(path.join(__dirname,'../../build')));
 
-server.use(BASE_PATH, express.static(path.join(__dirname,'build')));
 const port = process.env.PORT || 3000;
 server.get(
-    `${BASE_PATH}/internal/isAlive`,
+    `${BASE_PATH}internal/isAlive`,
     (req, res) => res.sendStatus(200)
 );
 server.get(
-    `${BASE_PATH}/internal/isReady`,
+    `${BASE_PATH}internal/isReady`,
     (req, res) => res.sendStatus(200)
 );
 server.listen(port, () => {
