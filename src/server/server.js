3394da00c015a1b7f05ce40c2fb459e65c76e61e
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-11-26 10:00:29 +0100

    endret i server js

--- src/server/server.js
+++ src/server/server.js
@@ -1,18 +1,22 @@
 const path = require('path');
 const express = require('express');
-const BASE_PATH='/bedriftsoversikt-og-ansatte/';
+const BASE_PATH='/bedriftsoversikt-og-ansatte';
 const server = express();
+
 server.use(BASE_PATH, express.static(path.join(__dirname,'../../build')));
 
 const port = process.env.PORT || 3000;
 server.get(
-    `${BASE_PATH}internal/isAlive`,
+    `${BASE_PATH}/internal/isAlive`,
     (req, res) => res.sendStatus(200)
 );
 server.get(
-    `${BASE_PATH}internal/isReady`,
+    `${BASE_PATH}/internal/isReady`,
     (req, res) => res.sendStatus(200)
 );
+server.get(`${BASE_PATH}/*`, (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../build', 'index.html'));
+});
 server.listen(port, () => {
     console.log('Server listening on port', port);
 });
