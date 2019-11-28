5b6975805fc9f4125435951be2b303733adbf371
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-11-28 13:33:10 +0100

    begynt a ta inn bedriftsmenyen

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,26 +1,39 @@
 import React, {FunctionComponent} from 'react';
 import './App.css';
-import {BrowserRouter} from 'react-router-dom';
+import {BrowserRouter, Switch} from 'react-router-dom';
 
 import { Route} from 'react-router';
 import InformasjonOmBedriftOgAnsatte from "./InformasjonOmBedriftOgAnsatte/InformasjonOmBedriftOgAnsatte";
 import { basename } from './paths';
+import InformasjonOmBedrift from "./InformasjonOmBedriftOgAnsatte/InformasjonOmBedrift/InformasjonOmBedrift";
+import MineAnsatte from "./InformasjonOmBedriftOgAnsatte/MineAnsatte/MineAnsatte";
 
 const App: FunctionComponent = () => {
 
-
-  return (
+    return (
       <div className="typo-normal">
           <div
           >helloo</div>
         <BrowserRouter basename={basename}>
           <div>
               <div className="bakgrunnsside">
+                  <Switch>
                           <Route
                               path="/"
                               exact={true}
                               component={InformasjonOmBedriftOgAnsatte}
                           />
+                      <Route
+                          path="/"
+                          exact={true}
+                          component={InformasjonOmBedrift}
+                      />
+                      <Route
+                          path="/ansatte"
+                          exact={true}
+                          component={MineAnsatte}
+                      />
+                  </Switch>
               </div>
           </div>
         </BrowserRouter>
@@ -29,3 +42,4 @@ const App: FunctionComponent = () => {
 };
 
 export default App;
+
