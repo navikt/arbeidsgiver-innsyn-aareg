0e84b39501dbccecd34e07e912363e476119f0a3
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-11-29 14:33:24 +0100

    hent orgfunskjon kallt for test

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,8 +5,10 @@ import {BrowserRouter} from 'react-router-dom';
 import { Route} from 'react-router';
 import InformasjonOmBedriftOgAnsatte from "./InformasjonOmBedriftOgAnsatte/InformasjonOmBedriftOgAnsatte";
 import { basename } from './paths';
+import {hentOrganisasjonerFraAltinn} from "../api/altinnApi";
 
 const App: FunctionComponent = () => {
+    hentOrganisasjonerFraAltinn();
 
 
   return (
