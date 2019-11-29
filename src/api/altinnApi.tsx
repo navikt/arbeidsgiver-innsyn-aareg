1863786c8a2d0561454c2687975df2d31707e89a
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-11-29 14:30:31 +0100

    lagt til oppsett for sonekryssing

--- src/api/altinnApi.tsx
+++ src/api/altinnApi.tsx
@@ -3,6 +3,7 @@ import {OrganisasjonFraAltinn} from "../App/Objekter/OrganisasjonFraAltinn";
 export async function hentOrganisasjonerFraAltinn(): Promise<OrganisasjonFraAltinn[]> {
     let respons = await fetch('/min-side-arbeidsgiver/api/organisasjoner');
     if (respons.ok) {
+        console.log(await respons.json);
         return await respons.json();
     } else {
         return [];
