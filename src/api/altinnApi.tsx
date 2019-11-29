835b331271f647ea523c0c1885ef212403075095
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-11-29 14:48:45 +0100

    endret til url i altinnkallet

--- src/api/altinnApi.tsx
+++ src/api/altinnApi.tsx
@@ -1,7 +1,7 @@
 import {OrganisasjonFraAltinn} from "../App/Objekter/OrganisasjonFraAltinn";
 
 export async function hentOrganisasjonerFraAltinn(): Promise<OrganisasjonFraAltinn[]> {
-    let respons = await fetch('/min-side-arbeidsgiver/api/organisasjoner');
+    let respons = await fetch('/bedriftsoversikt-og-ansatte/api/organisasjoner');
     if (respons.ok) {
         console.log(await respons.json);
         return await respons.json();
