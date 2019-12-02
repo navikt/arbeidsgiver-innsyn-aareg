de7db9c45a4466f6afd56013415cf75eb7d38fcd
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-02 15:10:25 +0100

    redigert craco config til a ha riktig path rewrite

--- src/server/server.js
+++ src/server/server.js
@@ -10,6 +10,11 @@ server.get(
     `${BASE_PATH}/internal/isAlive`,
     (req, res) => res.sendStatus(200)
 );
+server.get(`${BASE_PATH}/redirect-til-login`, (req, res) => {
+    const loginUrl = process.env.LOGIN_URL ||
+        'http://localhost:8080/ditt-nav-arbeidsgiver-api/local/selvbetjening-login?redirect=http://localhost:3000/min-side-arbeidsgiver';
+    res.redirect(loginUrl);
+});
 server.get(
     `${BASE_PATH}/internal/isReady`,
     (req, res) => res.sendStatus(200)
