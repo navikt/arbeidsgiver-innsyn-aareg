f390a06e9a37c792bae40c785deb6c07fc2ae409
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-04 13:14:49 +0100

    fatt inn alle oakker

--- src/App/MineAnsatte/Sokefelt/Sokefelt.tsx
+++ src/App/MineAnsatte/Sokefelt/Sokefelt.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { Input } from 'nav-frontend-skjema';
-
 import Forstørrelsesglass from './Forstørrelsesglass';
 import Kryss from './Kryss';
 import './Sokefelt.less';
