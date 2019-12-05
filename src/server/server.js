e182ac7f355aa2888e1a2b00ffe5db255b1debba
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-05 09:37:44 +0100

    lagt til server script

--- src/server/server.js
+++ src/server/server.js
@@ -2,8 +2,14 @@ const path = require('path');
 const express = require('express');
 const BASE_PATH='/bedriftsoversikt-og-ansatte';
 const server = express();
+const createEnvSettingsFile = require('./envSettings.js');
+const buildPath = path.join(__dirname,'../../build');
 
 server.use(BASE_PATH, express.static(path.join(__dirname,'../../build')));
+server.use(`${BASE_PATH}/api`, sonekrysning);
+server.use(`${BASE_PATH}/veilarbstepup/status`,veilarbStatusProxyConfig);
+
+createEnvSettingsFile(path.resolve(`${buildPath}/static/js/settings.js`));
 
 const port = process.env.PORT || 3000;
 server.get(
