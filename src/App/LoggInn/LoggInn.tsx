a8786db9c7e792c50f085867ea37c177ed50d2a2
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-05 09:48:31 +0100

    lagt til logging for a se om ,iljovariabler kan leses

--- src/App/LoggInn/LoggInn.tsx
+++ src/App/LoggInn/LoggInn.tsx
@@ -14,7 +14,9 @@ const LoggInn: FunctionComponent = () => {
     const redirectTilLogin = () => {
         if (environment.MILJO === 'prod-sbs' || environment.MILJO === 'dev-sbs') {
             window.location.href = '/bedriftsoversikt-og-ansatte/redirect-til-login';
+            console.log("Klart å lese miljø, miljø er lik: ", environment.MILJO);
         } else {
+            console.log("lokalKjoring");
             document.cookie = 'selvbetjening-idtoken =0123456789..*; path=/;';
             window.location.href = '/bedriftsoversikt-og-ansatte/';
         }
