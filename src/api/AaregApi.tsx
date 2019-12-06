03e90ae6ffba6396ffc5ac36efe72cbd905d7ff7
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-06 13:50:45 +0100

    lagt til headers i kall til backend

--- src/api/AaregApi.tsx
+++ src/api/AaregApi.tsx
@@ -1,7 +1,10 @@
 import {hentArbeidsforholdLink} from "../App/lenker";
 
-export async function hentArbeidsforholdFraAAreg(orgnr: string): Promise<any> {
-    let respons = await fetch(hentArbeidsforholdLink(orgnr));
+export async function hentArbeidsforholdFraAAreg(underenhet: string, enhet: string): Promise<any> {
+    const headere = new Headers();
+    headere.append('org-nr', underenhet);
+    headere.append('jurenhet', enhet);
+    let respons = await fetch(hentArbeidsforholdLink(), {headers:headere});
     if (respons.ok) {
         return await respons.json();
     }else {
