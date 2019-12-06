da2821e281a40187bc5a1c9b7ce11451bd9e6510
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-06 12:04:04 +0100

    fjernet dobbelrendring av komponent

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -11,7 +11,6 @@ const App: FunctionComponent = () => {
             <LoginBoundary>
                 <MineAnsatte/>
             </LoginBoundary>
-            <MineAnsatte/>
         </BrowserRouter>
   );
 };
