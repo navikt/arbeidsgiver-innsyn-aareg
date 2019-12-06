6dd9d78b1349a75d55a01815d8f12c94fc1164f4
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-06 12:37:33 +0100

    fjernet ubrukt import

--- src/App/MineAnsatte/Sokefelt/Sokefelt.tsx
+++ src/App/MineAnsatte/Sokefelt/Sokefelt.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from 'react';
 import { Input } from 'nav-frontend-skjema';
-import Kryss from './Kryss';
 import './Sokefelt.less';
 import Forstorrelsesglass from "./Forstørrelsesglass";
 
