4c90c6cb56c8639aae8851d3f7fd9bb7ef59f092
Ibenholt <bendik.segrov.ibenholt@nav.no>
2019-12-17 09:36:56 +0100

    la til prettier

--- src/App/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
+++ src/App/MineAnsatte/TabellMineAnsatte/TabellMineAnsatte.tsx
@@ -4,10 +4,8 @@ import KolonnerFullSkjerm from './Kolonner/Kolonner';
 import './TabellMineAnsatte.less';
 import 'nav-frontend-tabell-style';
 import { KolonneState } from '../MineAnsatte';
-import {Arbeidsforhold} from "../../Objekter/ArbeidsForhold";
-import {Link} from "react-router-dom";
-
-
+import { Arbeidsforhold } from '../../Objekter/ArbeidsForhold';
+import { Link } from 'react-router-dom';
 
 interface Props {
     className?: string;
@@ -15,19 +13,32 @@ interface Props {
     setNavarendeKolonne: (kolonne: KolonneState) => void;
     byttSide: (indeks: number) => void;
     navarendeKolonne: KolonneState;
-    settValgtArbeidsgiver:(indeks: number) => void;
-    valgtBedrift:string
+    settValgtArbeidsgiver: (indeks: number) => void;
+    valgtBedrift: string;
 }
 
 const TabellMineAnsatte: FunctionComponent<Props> = props => {
-    function oppdaterValgtArbeidsgiver (fnr:string){
+    function oppdaterValgtArbeidsgiver(fnr: string) {
         const fnrSomheltall: number = parseInt(fnr);
         props.settValgtArbeidsgiver(fnrSomheltall);
     }
     const rader = props.listeMedArbeidsForhold.map(arbeidsforhold => {
         return (
             <tr key={arbeidsforhold.navArbeidsforholdId}>
-                <td className={'td'}><div onClick={() => oppdaterValgtArbeidsgiver(arbeidsforhold.arbeidstaker.offentligIdent)}><Link  to={"enkeltarbeidsforhold/?bedrift=" + props.valgtBedrift +"&arbeidsforhold="+arbeidsforhold.navArbeidsforholdId}>{arbeidsforhold.arbeidstaker.navn}</Link></div></td>
+                <td className={'td'}>
+                    <div onClick={() => oppdaterValgtArbeidsgiver(arbeidsforhold.arbeidstaker.offentligIdent)}>
+                        <Link
+                            to={
+                                'enkeltarbeidsforhold/?bedrift=' +
+                                props.valgtBedrift +
+                                '&arbeidsforhold=' +
+                                arbeidsforhold.navArbeidsforholdId
+                            }
+                        >
+                            {arbeidsforhold.arbeidstaker.navn}
+                        </Link>
+                    </div>
+                </td>
                 <td className={'td'}>{arbeidsforhold.arbeidstaker.offentligIdent}</td>
                 <td className={'td'}>{arbeidsforhold.yrke}</td>
                 <td className={'td'}>{arbeidsforhold.ansattFom}</td>
