e39e6002139885cb1fae084e5424492b97b993a2
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2019-12-18 10:08:05 +0100

    fikset chevron i filtreringsmeny

--- src/App/MineAnsatte/NedtrekksMenyForFiltrering/NedtrekksMenyForFiltrering.tsx
+++ src/App/MineAnsatte/NedtrekksMenyForFiltrering/NedtrekksMenyForFiltrering.tsx
@@ -1,15 +1,23 @@
-import React, {FunctionComponent} from "react";
+import React, {FunctionComponent, useState} from "react";
 import {Button, Menu, MenuItem, Wrapper} from "react-aria-menubutton";
 import './NedtrekksMenyForFiltrering.less';
+import {NedChevron, OppChevron} from "nav-frontend-chevron";
 
 interface Props {
     onFiltrering: (value: any, event: any) => void;
 }
 
 const NedtrekksMenyForFiltrering: FunctionComponent<Props> = ({ onFiltrering }) => {
-    return (<Wrapper className="wrapper" onSelection={onFiltrering}>
+    const [erApen, setErApen] = useState(false);
+
+    return (<Wrapper className="wrapper" onSelection={onFiltrering} onMenuToggle={({ isOpen }) => {
+            setErApen(isOpen);
+        }}>
             <Button className="wrapper__button">
                 Vis alle
+                { !erApen &&
+                <NedChevron type={'ned'} />}
+                { erApen && <OppChevron/>}
             </Button>
             <Menu className="wrapper">
                 <MenuItem className="wrapper__valg" value={'visAlle'}>
