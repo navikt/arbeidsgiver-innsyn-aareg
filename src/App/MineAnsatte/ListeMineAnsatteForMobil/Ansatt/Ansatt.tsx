135883cf8f3a042a42b8f4342c1c30ba7d4c1a10
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-01-10 14:28:33 +0100

    Lenke til enkelt arbeidforhold for mobil (#11)
    * la til lenkt til enkelt arbeidsforhold for mobil
    
    * endret tittel på app
    
    * formattering og fjernet masse rødt i console

--- src/App/MineAnsatte/ListeMineAnsatteForMobil/Ansatt/Ansatt.tsx
+++ src/App/MineAnsatte/ListeMineAnsatteForMobil/Ansatt/Ansatt.tsx
@@ -2,6 +2,9 @@ import React, { FunctionComponent } from 'react';
 import './Ansatt.less';
 
 import AttributtVisning from './AttributtVisning/AttributtVisning';
+import {Link} from "react-router-dom";
+import {Arbeidstaker} from "../../../Objekter/Arbeidstaker";
+import {Arbeidsforhold} from "../../../Objekter/ArbeidsForhold";
 
 interface Props {
     className?: string;
@@ -10,13 +13,44 @@ interface Props {
     yrke: string;
     fom: string;
     tom: string;
+    arbeidsforhold:Arbeidsforhold
+    settValgtArbeidsgiver: (valgtArbeidstaker: Arbeidstaker) => void;
+    valgtBedrift: string;
 }
 
 const Ansatt: FunctionComponent<Props> = props => {
+    function oppdaterValgtArbeidsgiver(fnr: string, navn: string) {
+        const fnrSomheltall: number = parseInt(fnr);
+        props.settValgtArbeidsgiver({ fnr: fnrSomheltall, navn: navn });
+    }
+
     return (
         <li className="arbeidsforhold">
             <ul className="arbeidsforhold__liste">
-                <AttributtVisning attributt="Navn" attributtVerdi={props.navn} />
+                <div>
+                <li className="attributt">
+                    <div className={'attributt__navn'}> Navn</div>
+                    <div
+                        onClick={() =>
+                            oppdaterValgtArbeidsgiver(
+                                props.arbeidsforhold.arbeidstaker.offentligIdent,
+                                props.arbeidsforhold.arbeidstaker.navn
+                            )
+                        }
+                    >
+                        <Link
+                            to={
+                                'enkeltarbeidsforhold/?bedrift=' +
+                                props.valgtBedrift +
+                                '&arbeidsforhold=' +
+                                props.arbeidsforhold.navArbeidsforholdId
+                            }
+                        >
+                            {props.arbeidsforhold.arbeidstaker.navn}
+                        </Link>
+                    </div>
+                </li>
+                    </div>
                 <AttributtVisning attributt="Offentlig Ident" attributtVerdi={props.offentligID} />
                 <AttributtVisning attributt="Yrke" attributtVerdi={props.yrke} />
                 <AttributtVisning attributt="Startet" attributtVerdi={props.fom} />
