3c884b87671747db0ed697e04b7e4ae0b9c5a2ed
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-01-14 13:30:25 +0100

    formatering + fjernet ubrukt import

--- src/App/MineAnsatte/ExcelEksport/ExcelEksport.tsx
+++ src/App/MineAnsatte/ExcelEksport/ExcelEksport.tsx
@@ -1,52 +1,48 @@
-import React,{FunctionComponent} from "react";
-import {Hovedknapp} from 'nav-frontend-knapper';
+import React, { FunctionComponent } from 'react';
+import { Hovedknapp } from 'nav-frontend-knapper';
 // @ts-ignore
-import ReactExport from "react-data-export";
-import {Arbeidsforhold} from "../../Objekter/ArbeidsForhold";
-import {filtrerAktiveOgAvsluttede} from "../sorteringOgFiltreringsFunksjoner";
+import ReactExport from 'react-data-export';
+import { Arbeidsforhold } from '../../Objekter/ArbeidsForhold';
+import { filtrerAktiveOgAvsluttede } from '../sorteringOgFiltreringsFunksjoner';
 const ExcelFile = ReactExport.ExcelFile;
 const ExcelSheet = ReactExport.ExcelFile.ExcelSheet;
-const ExcelColumn = ReactExport.ExcelFile.ExcelColumn;
-
-
 
 type ExcelEksportProps = {
-    arbeidsforholdListe: Arbeidsforhold[],
-    className?:string,
-    navnBedrift:string,
-    orgnrBedrift:string
+    arbeidsforholdListe: Arbeidsforhold[];
+    className?: string;
+    navnBedrift: string;
+    orgnrBedrift: string;
+};
 
-}
-
-const convertToDataset = (arbeidsforhold:Arbeidsforhold[]) =>{
-    const arbeidsforholdDataset: string[][]=[];
+const convertToDataset = (arbeidsforhold: Arbeidsforhold[]) => {
+    const arbeidsforholdDataset: string[][] = [];
     arbeidsforhold.forEach(a => {
-        const detteArbeidsforholdet:Array<string> = [];
+        const detteArbeidsforholdet: Array<string> = [];
         detteArbeidsforholdet.push(a.arbeidstaker.navn);
         detteArbeidsforholdet.push(a.arbeidstaker.offentligIdent);
         detteArbeidsforholdet.push(a.yrke);
         detteArbeidsforholdet.push(a.ansattFom);
         detteArbeidsforholdet.push(a.ansattTom);
-        arbeidsforholdDataset.push(detteArbeidsforholdet)
+        arbeidsforholdDataset.push(detteArbeidsforholdet);
     });
 
-  console.log(arbeidsforholdDataset);
-    return arbeidsforholdDataset
+    console.log(arbeidsforholdDataset);
+    return arbeidsforholdDataset;
 };
 
 const kolonner = [
-    {title: "Navn", width: {wch: 25}},//pixels width
-    {title: "Fødselsnummer", width: {wch: 14}},//char width
-    {title: "Yrke", width: {wch: 20}},
-    {title: "Startdato", width: {wch: 13}},
-    {title: "Sluttdato", width: {wch: 13}},
+    { title: 'Navn', width: { wch: 25 } }, //pixels width
+    { title: 'Fødselsnummer', width: { wch: 14 } }, //char width
+    { title: 'Yrke', width: { wch: 20 } },
+    { title: 'Startdato', width: { wch: 13 } },
+    { title: 'Sluttdato', width: { wch: 13 } }
 ];
 
-const ExcelEksport: FunctionComponent<ExcelEksportProps> = (props:ExcelEksportProps) => {
-    const dagensDato:Date = new Date();
-    const aktiveArbeidsforhold = filtrerAktiveOgAvsluttede(props.arbeidsforholdListe,true);
+const ExcelEksport: FunctionComponent<ExcelEksportProps> = (props: ExcelEksportProps) => {
+    const dagensDato: Date = new Date();
+    const aktiveArbeidsforhold = filtrerAktiveOgAvsluttede(props.arbeidsforholdListe, true);
     const aktiveArbeidsforholdDataset = convertToDataset(aktiveArbeidsforhold);
-    const avsluttedeArbeidsforhold = filtrerAktiveOgAvsluttede(props.arbeidsforholdListe,false);
+    const avsluttedeArbeidsforhold = filtrerAktiveOgAvsluttede(props.arbeidsforholdListe, false);
     const avsluttedeArbeidsforholdDataset = convertToDataset(avsluttedeArbeidsforhold);
     const avsluttedeArbeidsforholdMultiDataSet = [
         {
@@ -60,12 +56,24 @@ const ExcelEksport: FunctionComponent<ExcelEksportProps> = (props:ExcelEksportPr
             data: aktiveArbeidsforholdDataset
         }
     ];
-    return <div className={props.className} >
-        <ExcelFile element={<Hovedknapp>Last Ned</Hovedknapp>} filename={"ANSATTFORHOLD_"+props.navnBedrift+"_"+props.orgnrBedrift+"_"+dagensDato.toLocaleDateString()}>
-            <ExcelSheet dataSet={ aktiveArbeidsforholdMultiDataSet} name="Aktive arbeidsforhold"/>
-            <ExcelSheet dataSet={ avsluttedeArbeidsforholdMultiDataSet} name="Avsluttede arbeidsforhold"/>
-        </ExcelFile>
-    </div>
+    return (
+        <div className={props.className}>
+            <ExcelFile
+                element={<Hovedknapp>Last Ned</Hovedknapp>}
+                filename={
+                    'ANSATTFORHOLD_' +
+                    props.navnBedrift +
+                    '_' +
+                    props.orgnrBedrift +
+                    '_' +
+                    dagensDato.toLocaleDateString()
+                }
+            >
+                <ExcelSheet dataSet={aktiveArbeidsforholdMultiDataSet} name="Aktive arbeidsforhold" />
+                <ExcelSheet dataSet={avsluttedeArbeidsforholdMultiDataSet} name="Avsluttede arbeidsforhold" />
+            </ExcelFile>
+        </div>
+    );
     /*
    return <div className={props.className} >
         <ExcelFile element={<Hovedknapp>Last Ned</Hovedknapp>} filename={"ANSATTFORHOLD_"+props.navnBedrift+"_"+props.orgnrBedrift+"_"+dagensDato.toLocaleDateString()} >
@@ -87,6 +95,4 @@ const ExcelEksport: FunctionComponent<ExcelEksportProps> = (props:ExcelEksportPr
    </div>*/
 };
 
-
-
-export default ExcelEksport
+export default ExcelEksport;
