104c81d6ca09fd75614d060703bda812c1290896
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-01-20 15:09:09 +0100

    satt pa styling pa filtrering pa varsler

--- src/App/MineAnsatte/Filtervalg/Filtervalg.tsx
+++ src/App/MineAnsatte/Filtervalg/Filtervalg.tsx
@@ -1,17 +1,19 @@
 import React, {FunctionComponent, SyntheticEvent} from 'react';
-import {ToggleGruppe, ToggleKnappPureProps} from "nav-frontend-toggle";
+import {ToggleGruppe, ToggleKnappPure, ToggleKnappPureProps} from "nav-frontend-toggle";
+import './Filtervalg.less';
 
 interface Props {
     filtreringValgt: (event: SyntheticEvent<EventTarget>,toggles: ToggleKnappPureProps[]) => void;
     overSiktOverAntallAktiveOgInaktive: number[]
+    setfiltrerPaVarsler: () => void
 }
 
 const Filtervalg: FunctionComponent<Props> = props => {
         const arrayMedToggleTekst = ['Alle(' + props.overSiktOverAntallAktiveOgInaktive[0]+")",'Aktive('+ props.overSiktOverAntallAktiveOgInaktive[1].toString()+")", 'Avsluttede('+ props.overSiktOverAntallAktiveOgInaktive[2].toString()+")" ];
-
         return (
+            <>
         <ToggleGruppe
-            onChange={props.filtreringValgt} className={"mine-ansatte__filter"}
+            onChange={props.filtreringValgt}
             defaultToggles={[
                 { children: arrayMedToggleTekst[0], pressed: true },
                 { children: arrayMedToggleTekst[1] },
@@ -19,6 +21,10 @@ const Filtervalg: FunctionComponent<Props> = props => {
             ]}
             minstEn
         />
+        <div className={ "mine-ansatte__varsel-filter"}>
+            <ToggleKnappPure children = {"varslinger"} onClick={props.setfiltrerPaVarsler} />
+            </div>
+            </>
     );
 };
 
