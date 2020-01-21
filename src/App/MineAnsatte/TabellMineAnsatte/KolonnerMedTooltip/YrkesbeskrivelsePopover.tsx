ba310f8018b9d1332da9aa3fd800063530e7777f
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-01-21 17:34:04 +0100

    lagt til referanse men finner ikke ref til yrkesbeskrivelseelement

--- src/App/MineAnsatte/TabellMineAnsatte/KolonnerMedTooltip/YrkesbeskrivelsePopover.tsx
+++ src/App/MineAnsatte/TabellMineAnsatte/KolonnerMedTooltip/YrkesbeskrivelsePopover.tsx
@@ -10,14 +10,21 @@ type PopoverProps = {
 
 const YrkesbeskrivelsePopover: FunctionComponent<PopoverProps> = (props:PopoverProps) => {
     const [anker, setAnker] = useState<HTMLElement | undefined>(undefined);
+    const ref = React.createRef<HTMLDivElement>();
+    if (ref.current) {
+        console.log(ref.current.offsetHeight, "Har hoyden" );
+    }
+
     return (
-        <div>
-            <Normaltekst className={props.className} onMouseEnter={(e: any) => setAnker(e.currentTarget)}
+
+        <div ref = {ref}>
+            <Normaltekst  className={props.className} onMouseEnter={(e: any) => setAnker(e.currentTarget)}
                  onMouseLeave={(e: any) => setAnker(undefined)}>{props.tekst}</Normaltekst>
             <Popover ankerEl={anker} orientering={PopoverOrientering.Over}>
                 <p  style={{padding: '1rem'}} >{props.tekst} </p>
             </Popover>
         </div>
+
     );
 };
 
