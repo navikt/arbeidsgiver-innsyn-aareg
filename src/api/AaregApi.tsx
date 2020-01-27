29aa70091edefb756ce96c5f86436d1ff676878c
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-01-27 09:25:24 +0100

    lagt til printing av miljovariabel i logging

--- src/api/AaregApi.tsx
+++ src/api/AaregApi.tsx
@@ -1,6 +1,7 @@
 import { hentArbeidsforholdLink } from '../App/lenker';
 import { ObjektFraAAregisteret, tomResponsFraAareg } from '../App/Objekter/ObjektFraAAreg';
 import amplitude from "../utils/amplitude";
+import environment from "../utils/environment";
 
 export async function hentArbeidsforholdFraAAreg(underenhet: string, enhet: string): Promise<ObjektFraAAregisteret> {
     const headere = new Headers();
@@ -9,11 +10,11 @@ export async function hentArbeidsforholdFraAAreg(underenhet: string, enhet: stri
     const startTtid = new Date();
     let respons = await fetch(hentArbeidsforholdLink(), { headers: headere });
     if (respons.ok) {
-        amplitude.logEvent("tok :" + (new Date().getTime() - startTtid.getTime()) / 1000 + "sekunder å hente arbeidsforhold");
+        amplitude.logEvent("tok: " + (new Date().getTime() - startTtid.getTime()) / 1000 + " sekunder å hente arbeidsforhold i miljoet: " + environment.MILJO);
         return await respons.json();
     } else {
         amplitude.logEvent("klarte ikke hente ut arbeidsforhold");
-        amplitude.logEvent("tok :" + (new Date().getTime() - startTtid.getTime()) / 1000 + "før kallet feilet");
+        amplitude.logEvent("tok: " + (new Date().getTime() - startTtid.getTime()) / 1000 + " før kallet feilet i miljøet " + environment.MILJO);
         return tomResponsFraAareg;
     }
 }
