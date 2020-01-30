dfe35073fdbfaf770fa0421c87a9b5adf3aa3bf8
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-01-30 13:25:27 +0100

    Legg paa dekorator (#24)
    * la til dekoratør fragmenter og decorator.js, forsøker å hente dekoratør og redre i server.js også
    
    * kopierer build
    
    * omstrukturerte server.js slik at den ikke server uten dekoratør som default

--- src/server/server.js
+++ src/server/server.js
@@ -2,28 +2,50 @@ const path = require('path');
 const express = require('express');
 const BASE_PATH = '/arbeidsforhold';
 const server = express();
+const mustacheExpress = require('mustache-express');
+const getDecorator = require('./decorator');
+const Promise = require('promise');
 const createEnvSettingsFile = require('./envSettings.js');
-const buildPath = path.join(__dirname, '../../build');
+
 const sonekrysning = require('./sonekrysningConfig.js');
 const veilarbStatusProxyConfig = require('./veilarbStatusProxyConfig');
 const enkeltArbeidsforholdProxyConfig = require('./enkeltArbeidsforholdProxy');
 
-server.use(BASE_PATH, express.static(path.join(__dirname, '../../build')));
+const buildPath = path.join(__dirname, '../../build');
+const port = process.env.PORT || 3000;
+
+
 server.use(`${BASE_PATH}/api`, sonekrysning);
 server.use(`${BASE_PATH}/veilarbstepup/status`, veilarbStatusProxyConfig);
 server.use(
     `${BASE_PATH}/person/arbeidsforhold-api/arbeidsforholdinnslag/arbeidsgiver`,
     enkeltArbeidsforholdProxyConfig
 );
+
+server.engine('html', mustacheExpress());
+server.set('view engine', 'mustache');
+server.set('views', buildPath);
+
 createEnvSettingsFile(path.resolve(`${buildPath}/static/js/settings.js`));
 
-const port = process.env.PORT || 3000;
 server.get(`${BASE_PATH}/redirect-til-login`, (req, res) => {
     const loginUrl =
         process.env.LOGIN_URL ||
         'http://localhost:8080/ditt-nav-arbeidsgiver-api/local/selvbetjening-login?redirect=http://localhost:3000/arbeidsforhold';
     res.redirect(loginUrl);
 });
+
+const renderApp = decoratorFragments =>
+    new Promise((resolve, reject) => {
+        server.render('index.html', decoratorFragments, (err, html) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(html);
+            }
+        });
+    });
+
 
 const startServer = html => {
     console.log('start server');
@@ -60,5 +82,13 @@ const setInternalEndpoints = () => {
 if (process.env.REACT_APP_MOCK) {
     startMockServer();
 } else {
-    startServer();
+    getDecorator()
+        .then(renderApp, error => {
+            console.error('Kunne ikke hente dekoratør ', error);
+            process.exit(1);
+        })
+        .then(startServer, error => {
+            console.error('Kunne ikke rendre app ', error);
+            process.exit(1);
+        })
 }
