9ff7b37ca73b378a39c0fd42bf9d311c3bfd3ce6
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-07 12:19:15 +0100

    excelfila vises ikke hvis lista er tom

--- src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
+++ src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
@@ -45,11 +45,11 @@ const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAar
     {((responsFraAaregisteret.length === 0 && <AlertStripeInfo className = {"mine-ansatte__informasjon"}>Det finnes ingen arbeidsforhold rapportert inn til Aa-registret etter 01.01.2015 for valgt underenhet. Dersom dette er feil må det rettes opp via a-meldingen.</AlertStripeInfo>)
         || (responsFraAaregisteret.length > 0 && <> <div className={'mine-ansatte__header'}>
             <AlertStripeInfo className = {"mine-ansatte__informasjon"}>Under finner du en oversikt over arbeidsforhold rapportert inn etter 01.01.2015. Dersom du finner feil eller mangler i oversikten skal disse korrigeres/rapporteres inn via a-meldingen. </AlertStripeInfo>
-            <ExcelEksport
+            {lengdeResponsFiltrertListe>0 && <ExcelEksport
                 arbeidsforholdListe={listeMedArbeidsforhold}
                 navnBedrift={valgtOrganisasjon.Name}
                 orgnrBedrift={valgtOrganisasjon.OrganizationNumber}
-            />
+            />}
         </div>
             <div className={'mine-ansatte__sok-og-filter'}>
                 <Normaltekst>Arbeidsforhold</Normaltekst>
