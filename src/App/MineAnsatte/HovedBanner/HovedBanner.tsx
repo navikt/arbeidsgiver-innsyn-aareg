7fa270e36c4cb51a38cb082ee78a89e572c5b7d0
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-02-07 12:22:21 +0100

    la til redirect til hovedside fra visning av enkelt arbforhold

--- src/App/MineAnsatte/HovedBanner/HovedBanner.tsx
+++ src/App/MineAnsatte/HovedBanner/HovedBanner.tsx
@@ -5,6 +5,7 @@ import '@navikt/bedriftsmeny/lib/bedriftsmeny.css';
 import './HovedBanner.less';
 import { Organisasjon } from '../../Objekter/OrganisasjonFraAltinn';
 import { JuridiskEnhetMedUnderEnheterArray } from '../../Objekter/JuridiskEnhetMedUnderenhetArray';
+import { basename } from '../../paths';
 
 interface Props extends RouteComponentProps {
     byttOrganisasjon: (org: Organisasjon) => void;
@@ -13,20 +14,31 @@ interface Props extends RouteComponentProps {
 const Banner: FunctionComponent<Props> = props => {
     const { history } = props;
 
+    const sjekkOmBrukerErPaaEnkeltArbeidsforholdSide = (organisasjon: Organisasjon) => {
+        const url = window.location.href;
+        if (url.indexOf('/enkeltarbeidsforhold') >= 0) {
+            redirectTilListeVisning(organisasjon);
+        }
+    };
+    const redirectTilListeVisning = (organisasjon: Organisasjon) => {
+        window.location.href = basename + '/?bedrift=' + organisasjon.OrganizationNumber;
+    };
+
     const onOrganisasjonChange = (organisasjon?: Organisasjon) => {
         if (organisasjon) {
+            sjekkOmBrukerErPaaEnkeltArbeidsforholdSide(organisasjon);
             props.byttOrganisasjon(organisasjon);
         }
     };
 
     return (
-        <div className={"hovebanner"} >
-        <Bedriftsmeny
-            sidetittel="Arbeidsforhold"
-            organisasjonstre={props.organisasjonstre}
-            onOrganisasjonChange={onOrganisasjonChange}
-            history={history}
-        />
+        <div className={'hovebanner'}>
+            <Bedriftsmeny
+                sidetittel="Arbeidsforhold"
+                organisasjonstre={props.organisasjonstre}
+                onOrganisasjonChange={onOrganisasjonChange}
+                history={history}
+            />
         </div>
     );
 };
