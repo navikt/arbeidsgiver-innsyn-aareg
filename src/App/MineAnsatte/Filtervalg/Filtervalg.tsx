d06d5c002c1e027382b5861be97c74c08c5f5bfa
Terese Haug <terese.haug@nav.no>
2020-02-12 12:23:35 +0100

    Oppdaterer styling for mobil og diverse refaktorering

--- src/App/MineAnsatte/Filtervalg/Filtervalg.tsx
+++ src/App/MineAnsatte/Filtervalg/Filtervalg.tsx
@@ -1,41 +1,54 @@
-import React, {FunctionComponent, SyntheticEvent, useState} from 'react';
-import {ToggleGruppe, ToggleKnappPure, ToggleKnappPureProps} from "nav-frontend-toggle";
+import React, { FunctionComponent, SyntheticEvent, useState } from 'react';
+import { ToggleGruppe, ToggleKnappPure, ToggleKnappPureProps } from 'nav-frontend-toggle';
 import './Filtervalg.less';
 
 interface Props {
-    filtreringValgt: (event: SyntheticEvent<EventTarget>,toggles: ToggleKnappPureProps[]) => void;
-    overSiktOverAntallAktiveOgInaktive: number[]
-    setfiltrerPaVarsler: () => void
+    filtreringValgt: (event: SyntheticEvent<EventTarget>, toggles: ToggleKnappPureProps[]) => void;
+    overSiktOverAntallAktiveOgInaktive: number[];
+    setfiltrerPaVarsler: () => void;
 }
 
 const Filtervalg: FunctionComponent<Props> = props => {
-
-    const klikkpaaFilterVarsel = ()=>{
+    const klikkpaaFilterVarsel = () => {
         props.setfiltrerPaVarsler();
         setFiltrertpaaVarsel(!filtrertPaaVarsel);
     };
 
-    const [filtrertPaaVarsel,setFiltrertpaaVarsel] = useState<boolean>(false);
-        const arrayMedToggleTekst = ['Alle (' + props.overSiktOverAntallAktiveOgInaktive[0] + ')','Aktive ('+ props.overSiktOverAntallAktiveOgInaktive[1].toString()+ ')', 'Avsluttede (' +props.overSiktOverAntallAktiveOgInaktive[2].toString()+ ')' ];
-        return (
+    const [filtrertPaaVarsel, setFiltrertpaaVarsel] = useState<boolean>(false);
 
-            <>
-                <div className={'togglecontainer'}>
-        <ToggleGruppe
-            onChange={props.filtreringValgt}
-            defaultToggles={[
-                { children: arrayMedToggleTekst[0], pressed: true },
-                { children: arrayMedToggleTekst[1] },
-                { children: arrayMedToggleTekst[2] }
-            ]}
-            minstEn
-        />
-        <div  className={'varselKnapp'} >
-            <ToggleKnappPure children = {"varslinger"} onClick={klikkpaaFilterVarsel } pressed = {filtrertPaaVarsel}/>
-        </div>
+    const arrayMedToggleTekst = [
+        'Alle (' + props.overSiktOverAntallAktiveOgInaktive[0] + ')',
+        'Aktive (' + props.overSiktOverAntallAktiveOgInaktive[1].toString() + ')',
+        `Avsluttede (${props.overSiktOverAntallAktiveOgInaktive[2].toString()})`
+    ];
+
+    /* const arrayMedToggleTekst = [
+        'Alle',
+        'Aktive',
+        `Avsluttede`
+    ];
+    const arrayMedToggleAntall = [
+        '(' + props.overSiktOverAntallAktiveOgInaktive[0] + ')',
+        '(' + props.overSiktOverAntallAktiveOgInaktive[1].toString() + ')',
+        `(${props.overSiktOverAntallAktiveOgInaktive[2].toString()})`
+    ];
+    */
+    return (
+        <div className={'togglecontainer'}>
+            <ToggleGruppe
+                onChange={props.filtreringValgt}
+                defaultToggles={[
+                    { children: arrayMedToggleTekst[0] , pressed: true },
+                    { children: arrayMedToggleTekst[1] },
+                    { children: arrayMedToggleTekst[2] }
+                ]}
+                minstEn
+                kompakt
+            />
+            <div className={'varselKnapp'}>
+                <ToggleKnappPure children={'varslinger'} onClick={klikkpaaFilterVarsel} pressed={filtrertPaaVarsel} />
             </div>
-            </>
-
+        </div>
     );
 };
 
