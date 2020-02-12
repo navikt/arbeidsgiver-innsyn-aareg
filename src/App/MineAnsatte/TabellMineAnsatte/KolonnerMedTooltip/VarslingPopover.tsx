9a0937afbc94a538cc31cc6ce597e1068dbadf9e
Ibenholt <bendik.segrov.ibenholt@nav.no>
2020-02-12 08:51:35 +0100

    la til key i varselpopover liste

--- src/App/MineAnsatte/TabellMineAnsatte/KolonnerMedTooltip/VarslingPopover.tsx
+++ src/App/MineAnsatte/TabellMineAnsatte/KolonnerMedTooltip/VarslingPopover.tsx
@@ -16,7 +16,7 @@ const VarslingPopover: FunctionComponent<PopoverProps> = (props:PopoverProps) =>
             <img src={varselikon} alt="Varsel om maskinell sluttdato" onMouseEnter={(e: any) => setAnker(e.currentTarget)}
                  onMouseLeave={(e: any) => setAnker(undefined)}/>
             <Popover ankerEl={anker}>
-                <div style={{padding: '1rem 1rem 0 1rem '}}>  {props.varsler.map(varsel => <div style={{padding: '0 0 1rem 0'}}>{varsel.varslingskodeForklaring}</div>)}</div>
+                <div style={{padding: '1rem 1rem 0 1rem '}}>  {props.varsler.map(varsel => <div key={varsel.varslingskode} style={{padding: '0 0 1rem 0'}}>{varsel.varslingskodeForklaring}</div>)}</div>
             </Popover>
         </div>
     );
