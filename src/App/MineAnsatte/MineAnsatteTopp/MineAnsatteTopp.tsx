bdf33b3fb1bf63ad8e74fd5d343ee2a83844f61a
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-14 13:14:56 +0100

    fikset bug i props oppramsning i mineanattte -topp

--- src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
+++ src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
@@ -14,7 +14,6 @@ import {filtreringValgt, tellAntallAktiveOgInaktiveArbeidsforhold} from "../sort
 
 interface Props {
     responsFraAaregisteret: Arbeidsforhold[];
-    lengdeResponsFiltrertListe: number;
     listeMedArbeidsforhold: Arbeidsforhold[];
     valgtOrganisasjon: Organisasjon;
     antallSider: number;
@@ -29,7 +28,7 @@ interface Props {
 };
 
 
-const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAaregisteret, lengdeResponsFiltrertListe,listeMedArbeidsforhold, valgtOrganisasjon, soketekst, antallVarsler, setFiltrerPaAktiveAvsluttede, antallSider ,setIndeksOgGenererListe, setSkalFiltrerePaVarsler, skalFiltrerePaVarsler}, naVarendeSidetall) => {
+const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAaregisteret, listeMedArbeidsforhold, valgtOrganisasjon, soketekst, antallVarsler, setFiltrerPaAktiveAvsluttede, antallSider ,setIndeksOgGenererListe, setSkalFiltrerePaVarsler, skalFiltrerePaVarsler, naVarendeSidetall} ) => {
 
     const onSoketekstChange = (soketekst: string) => {
         setSoketekst(soketekst);
@@ -45,7 +44,7 @@ const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAar
     {((responsFraAaregisteret.length === 0 && <AlertStripeInfo className = {"mine-ansatte__informasjon"}>Det finnes ingen arbeidsforhold rapportert inn til Aa-registret etter 01.01.2015 for valgt underenhet. Dersom dette er feil må det rettes opp via a-meldingen.</AlertStripeInfo>)
         || (responsFraAaregisteret.length > 0 && <> <div className={'mine-ansatte__header'}>
             <AlertStripeInfo className = {"mine-ansatte__informasjon"}>Under finner du en oversikt over arbeidsforhold rapportert inn etter 01.01.2015. Dersom du finner feil eller mangler i oversikten skal disse korrigeres/rapporteres inn via a-meldingen. </AlertStripeInfo>
-            {lengdeResponsFiltrertListe>0 && <ExcelEksport
+            {listeMedArbeidsforhold.length>0 && <ExcelEksport
                 arbeidsforholdListe={listeMedArbeidsforhold}
                 navnBedrift={valgtOrganisasjon.Name}
                 orgnrBedrift={valgtOrganisasjon.OrganizationNumber}
@@ -59,7 +58,7 @@ const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAar
             </div>
             <div className={'mine-ansatte__topp'}>
                 <div tabIndex={0} className={'mine-ansatte__antall-forhold'}>
-                    <Normaltekst>Viser {lengdeResponsFiltrertListe} av {responsFraAaregisteret.length} arbeidsforhold</Normaltekst>
+                    <Normaltekst>Viser {listeMedArbeidsforhold.length} av {responsFraAaregisteret.length} arbeidsforhold</Normaltekst>
                 </div>
                 {antallSider > 1 && <SideBytter
                     plassering={"overst"}
@@ -69,7 +68,7 @@ const MineAnsatteTopp: FunctionComponent<Props> = ({ setSoketekst, responsFraAar
                     naVarendeSidetall={naVarendeSidetall}
                 />}
             </div>
-        </>))} {( responsFraAaregisteret.length >0 && lengdeResponsFiltrertListe === 0 && <div className={"mine-ansatte__bytt-filtrering"}><Systemtittel>Finner ingen arbeidsforhold.</Systemtittel> <Ingress>Prøv å endre søkeord eller bytt filtreringsvalg.</Ingress></div>)}
+        </>))} {( responsFraAaregisteret.length > 0 && listeMedArbeidsforhold.length === 0 && <div className={"mine-ansatte__bytt-filtrering"}><Systemtittel>Finner ingen arbeidsforhold.</Systemtittel> <Ingress>Prøv å endre søkeord eller bytt filtreringsvalg.</Ingress></div>)}
         </>
 
 );
