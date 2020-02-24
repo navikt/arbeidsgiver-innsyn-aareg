fab1102bcb145b15127c077877f60d073120ff9f
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-24 15:26:24 +0100

    bruker riktig tid

--- src/App/MineAnsatte/Progressbar/Progressbar.tsx
+++ src/App/MineAnsatte/Progressbar/Progressbar.tsx
@@ -8,30 +8,28 @@ interface Props {
 }
 
 const Progressbar = ({ beregnetTid }: Props) => {
-
-    const [tidGjenstar, setTidGjenstar] = useState(beregnetTid);
-
-    console.log(beregnetTid);
-    console.log(tidGjenstar);
+    const [tidGatt, setTidGatt] = useState(0);
+    const [initialTid, setInitial] = useState(new Date().getTime());
 
     useEffect(() => {
+        console.log(tidGatt);
         const element = document.getElementById("progressbar__fyll");
-        if (tidGjenstar>0) {
+        if (tidGatt < beregnetTid) {
             if(element) {
-                setTimeout(  () =>{element.style.width =  (((beregnetTid - tidGjenstar)/beregnetTid)*100).toString() + "%";
-                    }, 300);
-                setTidGjenstar(tidGjenstar - (beregnetTid/100));
+                element.style.width =  ((tidGatt/beregnetTid)*100).toString() + "%";
+                setInitial(new Date().getTime());
+                console.log(tidGatt + ( new Date().getTime() - initialTid));
+                    setTidGatt(tidGatt + (new Date().getTime() - initialTid));
+                console.log(tidGatt);
             }
-
         }
-        }, [ beregnetTid,tidGjenstar]);
+        }, [ beregnetTid,tidGatt, initialTid]);
 
     return (
         <div className="progressbar">
             <div className={"progressbar__fyll"} id={"progressbar__fyll"} />
         </div>
     );
-}
-
+};
 
 export default Progressbar;
