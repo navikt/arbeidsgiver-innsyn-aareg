e324464dabfd66d8bd407077bdbdf13bb58eeb88
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-02-24 16:14:11 +0100

    lagd ferdig logikk sa progressbaren ikke hakker

--- src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
+++ src/App/MineAnsatte/MineAnsatteTopp/MineAnsatteTopp.tsx
@@ -56,7 +56,7 @@ const MineAnsatteTopp: FunctionComponent<Props> = (
     };
 
     return (
-        <><Progressbar beregnetTid={80000} onProgress={tid => {console.log(tid)}}/>
+        <><Progressbar beregnetTid={10000} startTid={new Date().getTime()} onProgress={tid => {console.log(tid)}}/>
             {responsFraAaregisteret.length === 0 ? (
                 <AlertStripeInfo className="mine-ansatte__informasjon">
                     Det finnes ingen arbeidsforhold rapportert inn til Aa-registret etter 01.01.2015 for valgt
