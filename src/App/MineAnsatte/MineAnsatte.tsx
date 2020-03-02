982ee98d145fc5368a8ec0a81c37fd76f9b61e79
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-03-02 11:41:20 +0100

    merget med master

--- src/App/MineAnsatte/MineAnsatte.tsx
+++ src/App/MineAnsatte/MineAnsatte.tsx
@@ -1,10 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { Normaltekst, Systemtittel } from 'nav-frontend-typografi';
+import React, {useEffect, useState} from 'react';
+import {Normaltekst, Systemtittel} from 'nav-frontend-typografi';
 import Lenke from 'nav-frontend-lenker';
-import { Organisasjon } from '../Objekter/OrganisasjonFraAltinn';
-import { Arbeidstaker } from '../Objekter/Arbeidstaker';
-import { Arbeidsforhold } from '../Objekter/ArbeidsForhold';
-import { byggListeBasertPaPArametere, sorterArbeidsforhold } from './sorteringOgFiltreringsFunksjoner';
+import {Organisasjon} from '../Objekter/OrganisasjonFraAltinn';
+import {Arbeidstaker} from '../Objekter/Arbeidstaker';
+import {Arbeidsforhold} from '../Objekter/ArbeidsForhold';
+import {byggListeBasertPaPArametere, sorterArbeidsforhold} from './sorteringOgFiltreringsFunksjoner';
 import {
     regnUtantallSider,
     regnUtArbeidsForholdSomSkalVisesPaEnSide,
@@ -13,11 +13,13 @@ import {
 import SideBytter from './SideBytter/SideBytter';
 import ListeMedAnsatteForMobil from './ListeMineAnsatteForMobil/ListeMineAnsatteForMobil';
 import TabellMineAnsatte from './TabellMineAnsatte/TabellMineAnsatte';
-import {hentAntallArbeidsforholdFraAareg, hentArbeidsforholdFraAAreg} from '../../api/AaregApi';
+import {hentAntallArbeidsforholdFraAareg, hentArbeidsforholdFraAAreg} from '../../api/aaregApi';
 import {linkTilMinSideArbeidsgiver} from '../lenker';
 import MineAnsatteTopp from './MineAnsatteTopp/MineAnsatteTopp';
 import './MineAnsatte.less';
+import {APISTATUS} from '../../api/api-utils';
 import Progressbar from "./Progressbar/Progressbar";
+import {AlertStripeFeil} from "nav-frontend-alertstriper";
 
 interface MineAnsatteProps {
     setValgtArbeidstaker: (arbeidstaker: Arbeidstaker) => void;
@@ -57,9 +59,10 @@ const MineAnsatte = (props: MineAnsatteProps) => {
 
     const [listeFraAareg, setListeFraAareg] = useState(Array<Arbeidsforhold>());
     const [antallArbeidsforhold, setAntallArbeidsforhold] = useState(0);
-    const [ferdiglastet, setFerdiglastet] = useState<boolean>(false);
     const [visProgressbar, setVisProgressbar] = useState(false);
 
+    const [aaregLasteState, setAaregLasteState] = useState<APISTATUS>(APISTATUS.LASTER);
+    const [feilkode, setFeilkode] = useState<string>('');
     const arbeidsforholdPerSide = 25;
 
     const setIndeksOgGenererListe = (indeks: number) => {
@@ -70,29 +73,32 @@ const MineAnsatte = (props: MineAnsatteProps) => {
         hentAntallArbeidsforholdFraAareg(props.valgtOrganisasjon.OrganizationNumber, props.valgtOrganisasjon.ParentOrganizationNumber).then(antall => {
             const antallForhold = antall.valueOf();
             if (antallForhold>0) {
-                setFerdiglastet(false);
+                setAaregLasteState(APISTATUS.LASTER);
                 setVisProgressbar(true);
             }
             else {
                 setVisProgressbar(false);
-                setFerdiglastet(true);
+                setAaregLasteState(APISTATUS.OK);
             }
             setAntallArbeidsforhold(antallForhold);
         })
     }, [props.valgtOrganisasjon]);
 
     useEffect(() => {
-
-        if (antallArbeidsforhold > 0) {
+        if (antallArbeidsforhold>0) {
             hentArbeidsforholdFraAAreg(
                 props.valgtOrganisasjon.OrganizationNumber,
                 props.valgtOrganisasjon.ParentOrganizationNumber
             ).then(responsAareg => {
                 setListeFraAareg(responsAareg.arbeidsforholdoversikter);
-                setFerdiglastet(true);
-            })
+                setAaregLasteState(APISTATUS.OK);
+            }).catch(error => {
+                setAaregLasteState(APISTATUS.FEILET);
+                setFeilkode(error.response.status.toString());
+            });
         }
     }, [props.valgtOrganisasjon, antallArbeidsforhold]);
+
 
     useEffect(() => {
         const oppdatertListe = byggListeBasertPaPArametere(
@@ -137,7 +143,9 @@ const MineAnsatte = (props: MineAnsatteProps) => {
         );
     }, [antallSider, naVarendeSidetall]);
 
-    console.log(listeFraAareg.length);
+    const feilmeldingtekst = feilkode === '408'
+        ? 'Det oppstod en feil da vi prøvde å hente dine arbeidsforhold. Prøv å laste siden på nytt eller kontakte brukerstøtte hvis problemet vedvarer.'
+        : 'Vi opplever ustabilitet med Aa-registret. Prøv å laste siden på nytt eller kontakte brukerstøtte hvis problemet vedvarer.';
 
     return (
         <div className="bakgrunnsside">
@@ -151,26 +159,28 @@ const MineAnsatte = (props: MineAnsatteProps) => {
                 <div className="mine-ansatte">
                     <Systemtittel className="mine-ansatte__systemtittel" tabIndex={0}>
                         Opplysninger fra Aa-registeret
-                    </Systemtittel>{antallArbeidsforhold > 0  && visProgressbar && (
-                    <Progressbar antall={antallArbeidsforhold} setSkalvises = {setVisProgressbar}  erFerdigLastet={ferdiglastet} startTid={new Date().getTime()} />
-                    )}{ (!visProgressbar) && <>
-                    {ferdiglastet && <MineAnsatteTopp
-                        valgtOrganisasjon={props.valgtOrganisasjon}
-                        setIndeksOgGenererListe={setIndeksOgGenererListe}
-                        setSoketekst={setSoketekst}
-                        antallSider={antallSider}
-                        antallVarsler={antallVarsler}
-                        lengdeResponsFiltrertListe={listeMedArbeidsForhold.length}
-                        listeMedArbeidsforhold={listeMedArbeidsForhold}
-                        naVarendeSidetall={naVarendeSidetall}
-                        responsFraAaregisteret={listeFraAareg}
-                        soketekst={soketekst}
-                        setSkalFiltrerePaVarsler={setSkalFiltrerePaVarsler}
-                        skalFiltrerePaVarsler={skalFiltrerePaVarsler}
-                        setFiltrerPaAktiveAvsluttede={setFiltrerPaAktiveAvsluttede}
-                    />}
+                    </Systemtittel>
+                    {antallArbeidsforhold > 0  && visProgressbar && (
+                    <Progressbar antall={antallArbeidsforhold} setSkalvises = {setVisProgressbar}  erFerdigLastet={aaregLasteState === APISTATUS.OK} startTid={new Date().getTime()} />)}
+                    {aaregLasteState === APISTATUS.OK && !visProgressbar &&(
+                        <MineAnsatteTopp
+                            valgtOrganisasjon={props.valgtOrganisasjon}
+                            setIndeksOgGenererListe={setIndeksOgGenererListe}
+                            setSoketekst={setSoketekst}
+                            antallSider={antallSider}
+                            antallVarsler={antallVarsler}
+                            lengdeResponsFiltrertListe={listeMedArbeidsForhold.length}
+                            listeMedArbeidsforhold={listeMedArbeidsForhold}
+                            naVarendeSidetall={naVarendeSidetall}
+                            responsFraAaregisteret={listeFraAareg}
+                            soketekst={soketekst}
+                            setSkalFiltrerePaVarsler={setSkalFiltrerePaVarsler}
+                            skalFiltrerePaVarsler={skalFiltrerePaVarsler}
+                            setFiltrerPaAktiveAvsluttede={setFiltrerPaAktiveAvsluttede}
+                        />
+                    )}
 
-                    {ferdiglastet && listeMedArbeidsForhold.length > 0 && (
+                    {aaregLasteState === APISTATUS.OK && listeMedArbeidsForhold.length > 0 && (
                         <>
                             {' '}
                             <TabellMineAnsatte
@@ -190,6 +200,11 @@ const MineAnsatte = (props: MineAnsatteProps) => {
                             />
                         </>
                     )}
+                    {aaregLasteState === APISTATUS.FEILET && (
+                        <div className="mine-ansatte__feilmelding-aareg">
+                            <AlertStripeFeil>{feilmeldingtekst}</AlertStripeFeil>
+                        </div>
+                    )}
                     {antallSider > 1 && (
                         <SideBytter
                             plassering="nederst"
@@ -199,7 +214,6 @@ const MineAnsatte = (props: MineAnsatteProps) => {
                             naVarendeSidetall={naVarendeSidetall}
                         />
                     )}
-                    </>}
                 </div>
             </div>
         </div>
---
+++ src/api/aaregApi.ts
@@ -0,0 +1,50 @@
+import { hentArbeidsforholdLink } from '../App/lenker';
+import { ObjektFraAAregisteret } from '../App/Objekter/ObjektFraAAreg';
+import {hentAntallArbeidsforholdLink} from '../App/lenker';
+
+import amplitude from '../utils/amplitude';
+import {
+    loggAntallAnsatte,
+    loggSnittTidPerArbeidsforhold,
+    loggTidForAlleArbeidsforhold
+} from '../App/amplitudefunksjonerForLogging';
+import { FetchError } from './api-utils';
+import {OversiktOverAntallForholdPerUnderenhet} from "../App/Objekter/OversiktOverAntallForholdPerUnderenhet";
+
+export async function hentArbeidsforholdFraAAreg(underenhet: string, enhet: string): Promise<ObjektFraAAregisteret> {
+    const headere = new Headers();
+    headere.set('orgnr', underenhet);
+    headere.set('jurenhet', enhet);
+    const startTtid = new Date();
+    let response: Response = await fetch(hentArbeidsforholdLink(), { headers: headere });
+
+    if (response.ok) {
+        const jsonRespons: ObjektFraAAregisteret = await response.json();
+        loggAntallAnsatte(jsonRespons.arbeidsforholdoversikter.length);
+        const tid = new Date().getDate() - startTtid.getDate();
+        loggSnittTidPerArbeidsforhold(jsonRespons.arbeidsforholdoversikter.length, tid);
+        loggTidForAlleArbeidsforhold(tid);
+        return jsonRespons;
+    } else {
+        amplitude.logEvent('#arbeidsforhold klarte ikke hente ut arbeidsforhold');
+        throw new FetchError(response.statusText || response.type, response);
+    }
+}
+
+export async function hentAntallArbeidsforholdFraAareg(underenhet: string, enhet: string): Promise<Number> {
+    const headere = new Headers();
+    headere.set('opplysningspliktig', enhet);
+    headere.set('orgnr', underenhet);
+    let respons = await fetch(hentAntallArbeidsforholdLink(), { headers: headere });
+    if (respons.ok) {
+        const jsonRespons: OversiktOverAntallForholdPerUnderenhet = await respons.json();
+        const valgtunderEnhet =  jsonRespons.filter(oversikt => oversikt.arbeidsgiver.organisasjonsnummer === underenhet);
+        if (valgtunderEnhet[0]) {
+            return valgtunderEnhet[0].aktiveArbeidsforhold + valgtunderEnhet[0].inaktiveArbeidsforhold;
+        }
+        return 0;
+    }
+    else {
+        return 0;
+    }
+}
