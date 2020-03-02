8b1f3c2a4d6218c5a036a21f1467b8b56e77b942
Terese Haug <terese.haug@nav.no>
2020-03-02 15:05:45 +0100

    Prettier

--- src/mocking/BeregnetTidForArbeidsforholdMock.ts
+++ src/mocking/BeregnetTidForArbeidsforholdMock.ts
@@ -1,5 +1,5 @@
 import fetchMock from 'fetch-mock';
-import {hentAntallArbeidsforholdLink} from '../App/lenker';
+import { hentAntallArbeidsforholdLink } from '../App/lenker';
 
 const delay = new Promise(res => setTimeout(res, 500));
 
@@ -13,6 +13,14 @@ fetchMock
     .spy();
 
 const mockObjekt = [
-    {"arbeidsgiver":{"organisasjonsnummer":"910825518","type":"Organisasjon"},"aktiveArbeidsforhold":1500,"inaktiveArbeidsforhold":200},
-    {"arbeidsgiver":{"organisasjonsnummer":"910825526","type":"Organisasjon"},"aktiveArbeidsforhold":500,"inaktiveArbeidsforhold":4},
+    {
+        arbeidsgiver: { organisasjonsnummer: '910825518', type: 'Organisasjon' },
+        aktiveArbeidsforhold: 1500,
+        inaktiveArbeidsforhold: 200
+    },
+    {
+        arbeidsgiver: { organisasjonsnummer: '910825526', type: 'Organisasjon' },
+        aktiveArbeidsforhold: 500,
+        inaktiveArbeidsforhold: 4
+    }
 ];
