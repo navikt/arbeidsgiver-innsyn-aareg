4be0a1ce892fbaf414a3640822c348d2720b2c16
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-05-29 13:22:57 +0200

    fikset lenke tilbake til msa

--- src/App/lenker.tsx
+++ src/App/lenker.tsx
@@ -27,8 +27,8 @@ export const hentOverordnetEnhetApiLink = (orgnr: string) => {
 
 export const linkTilMinSideArbeidsgiver = (orgnr: string) => {
     if (environment.MILJO === 'prod-sbs') {
-        return 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/' + orgnr + '/';
+        return 'https://arbeidsgiver.nav.no/min-side-arbeidsgiver/?bedrift=' + orgnr;
     } else {
-        return 'https://arbeidsgiver-q.nav.no/min-side-arbeidsgiver/' + orgnr + '/';
+        return 'https://arbeidsgiver-q.nav.no/min-side-arbeidsgiver/?bedrift=' + orgnr;
     }
 };
