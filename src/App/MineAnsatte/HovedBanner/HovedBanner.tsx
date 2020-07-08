5cc5abae21c68c1756ea48df83400668ffca4915
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-07-08 17:17:26 +0200

    alle parametere nullstilles ved bytte av bedrift men ikke refresh

--- src/App/MineAnsatte/HovedBanner/HovedBanner.tsx
+++ src/App/MineAnsatte/HovedBanner/HovedBanner.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React, {FunctionComponent, useState} from 'react';
 import { withRouter, RouteComponentProps } from 'react-router';
 import { Organisasjon } from '../../Objekter/OrganisasjonFraAltinn';
 import { basename } from '../../paths';
@@ -9,10 +9,12 @@ import './HovedBanner.less';
 interface Props extends RouteComponentProps {
     byttOrganisasjon: (org: Organisasjon) => void;
     organisasjoner: Organisasjon[];
+    url: string;
 }
 
 const Banner: FunctionComponent<Props> = props => {
     const { history } = props;
+
 
     const sjekkOmBrukerErPaaEnkeltArbeidsforholdSide = (organisasjon: Organisasjon) => {
         const url = window.location.href;
@@ -25,8 +27,23 @@ const Banner: FunctionComponent<Props> = props => {
         window.location.href = basename + '/?bedrift=' + organisasjon.OrganizationNumber;
     };
 
+    const nullStillUrl = () => {
+        const currentUrl = new URL(window.location.href);
+        currentUrl.searchParams.set("side", "1");
+        currentUrl.searchParams.set("filter", "Alle");
+        currentUrl.searchParams.set("varsler", "false");
+        currentUrl.searchParams.set("sok", "");
+        currentUrl.searchParams.set("sorter", "0");
+        currentUrl.searchParams.set("revers", "false");
+        const { search } = currentUrl;
+        history.replace(search);
+    }
+    const currentUrl = new URL(props.url);
     const onOrganisasjonChange = (organisasjon?: Organisasjon) => {
         if (organisasjon) {
+            if (organisasjon.OrganizationNumber !== currentUrl.searchParams.get("bedrift")) {
+                nullStillUrl()
+            }
             sjekkOmBrukerErPaaEnkeltArbeidsforholdSide(organisasjon);
             props.byttOrganisasjon(organisasjon);
         }
