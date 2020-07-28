38b4f18eeb8ae6f20d972a114f547d430c4c00a1
hilde.heggstad <hilde.heggstad@bekk.no>
2020-07-28 15:39:43 +0200

    lagt inn kall til ny backend i bakgrunnen

--- src/api/aaregApi.ts
+++ src/api/aaregApi.ts
@@ -1,4 +1,9 @@
-import {hentAntallArbeidsforholdLink, hentArbeidsforholdLink, sjekkSonekryssingLink} from '../App/lenker';
+import {
+    hentAntallArbeidsforholdLink,
+    hentArbeidsforholdLink,
+    hentArbeidsforholdLinkNyBackend,
+    sjekkSonekryssingLink
+} from '../App/lenker';
 import { ObjektFraAAregisteret } from '../App/Objekter/ObjektFraAAreg';
 import {
     loggAntallAnsatte,
@@ -15,6 +20,24 @@ export async function hentArbeidsforholdFraAAreg(underenhet: string, enhet: stri
     const startTtid = new Date();
     let response: Response = await fetch(hentArbeidsforholdLink(), { headers: headere, signal: signal });
 
+    if (response.ok) {
+        const jsonRespons: ObjektFraAAregisteret = await response.json();
+        loggAntallAnsatte(jsonRespons.arbeidsforholdoversikter.length);
+        const tid = new Date().getDate() - startTtid.getDate();
+        loggSnittTidPerArbeidsforhold(jsonRespons.arbeidsforholdoversikter.length, tid);
+        loggTidForAlleArbeidsforhold(tid);
+        return jsonRespons;
+    } else {
+        throw new FetchError(response.statusText || response.type, response);
+    }
+}
+
+export async function hentArbeidsforholdFraAAregNyBackend(underenhet: string, enhet: string, signal: any): Promise<ObjektFraAAregisteret> {
+    const headere = new Headers();
+    headere.set('orgnr', underenhet);
+    headere.set('jurenhet', enhet);
+    const startTtid = new Date();
+    let response: Response = await fetch(hentArbeidsforholdLinkNyBackend(), { headers: headere, signal: signal });
     if (response.ok) {
         const jsonRespons: ObjektFraAAregisteret = await response.json();
         loggAntallAnsatte(jsonRespons.arbeidsforholdoversikter.length);
