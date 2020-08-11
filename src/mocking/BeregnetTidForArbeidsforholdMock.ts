0fb5ddd8869f6daa73a713f2e4b1d729879edd93
hilde.heggstad <hilde.heggstad@bekk.no>
2020-08-11 11:41:32 +0200

    kaller arbeidsforhold med a bruke ny backend og lagt til ny mock

--- src/mocking/BeregnetTidForArbeidsforholdMock.ts
+++ src/mocking/BeregnetTidForArbeidsforholdMock.ts
@@ -1,36 +1,11 @@
 import fetchMock from 'fetch-mock';
-import { hentAntallArbeidsforholdLink } from '../App/lenker';
-
+import { hentAntallArbeidsforholdLinkNyBackend} from '../App/lenker';
 const delay = new Promise(res => setTimeout(res, 500));
-
 fetchMock
     .get(
-        hentAntallArbeidsforholdLink(),
+        hentAntallArbeidsforholdLinkNyBackend(),
         delay.then(() => {
-            return mockObjekt;
+            return {first: '910825518', second: 70};
         })
     )
     .spy();
-
-const mockObjekt = [
-    {
-        arbeidsgiver: { organisasjonsnummer: '910825518', type: 'Organisasjon' },
-        aktiveArbeidsforhold: 40,
-        inaktiveArbeidsforhold: 100
-    },
-    {
-        arbeidsgiver: { organisasjonsnummer: '910825526', type: 'Organisasjon' },
-        aktiveArbeidsforhold: 30,
-        inaktiveArbeidsforhold: 30
-    },
-    {
-        arbeidsgiver: { organisasjonsnummer: '910825607', type: 'Organisasjon' },
-        aktiveArbeidsforhold: 0,
-        inaktiveArbeidsforhold: 0
-    },
-    {
-        arbeidsgiver: { organisasjonsnummer: '910825569', type: 'Organisasjon' },
-        aktiveArbeidsforhold: 0,
-        inaktiveArbeidsforhold: 0
-    }
-];
