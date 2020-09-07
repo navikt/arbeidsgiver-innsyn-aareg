a2b6da64fefb65c401b91c7b528527be29dfa939
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-09-07 14:59:42 +0200

    select og flyt henger med

--- src/mocking/AaregMock.tsx
+++ src/mocking/AaregMock.tsx
@@ -1,12 +1,22 @@
 import fetchMock from 'fetch-mock';
-import { hentArbeidsforholdLinkNyBackend} from '../App/lenker';
+import {hentArbeidsforholdLinkNyBackend, hentTidligereVirksomheterLink} from '../App/lenker';
 import { AaregMockObjekt } from './funksjonerForAlageAAregMock';
+import {tidligerVirksomheter} from "./mockresponsFraAltinn";
 const delay = new Promise(res => setTimeout(res, 4000));
 fetchMock
     .get(
         hentArbeidsforholdLinkNyBackend(),
         delay.then(() => {
           return AaregMockObjekt;
+        })
+    )
+    .spy();
+
+fetchMock
+    .get(
+        hentTidligereVirksomheterLink,
+        delay.then(() => {
+            return tidligerVirksomheter;
         })
     )
     .spy();
