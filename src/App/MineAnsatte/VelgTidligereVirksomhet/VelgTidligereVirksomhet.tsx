41563180b907f3c3e2d7f0bb00fd7f53f9005ad3
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-09-08 12:56:09 +0200

    velger forte bedrift i lista over gamle bedrifter

--- src/App/MineAnsatte/VelgTidligereVirksomhet/VelgTidligereVirksomhet.tsx
+++ src/App/MineAnsatte/VelgTidligereVirksomhet/VelgTidligereVirksomhet.tsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './VelgTidligereVirksomhet.less';
 import {Organisasjon} from "../../Objekter/OrganisasjonFraAltinn";
 import {Select} from "nav-frontend-skjema";
+import environment from "../../../utils/environment";
+import amplitude from "../../../utils/amplitude";
+import {tidligerVirksomheter} from "../../../mocking/mockresponsFraAltinn";
 
 interface Props {
   tidligereOrganisasjoner?: Organisasjon[];
   setTidligereVirksomhet: (virksomhet: Organisasjon) => void;
+  tidligereVirksomhet?: Organisasjon;
 }
 
-const VelgTidligereVirksomhet = ({ tidligereOrganisasjoner, setTidligereVirksomhet}: Props) => {
+const VelgTidligereVirksomhet = ({ tidligereOrganisasjoner, setTidligereVirksomhet, tidligereVirksomhet}: Props) => {
     const objekter = tidligereOrganisasjoner && tidligereOrganisasjoner.map((virksomhet) => {
     return <option  value={virksomhet.OrganizationNumber} key={virksomhet.OrganizationNumber}
     >{virksomhet.Name}</option>
   })
+
+    useEffect(() => {
+        if (!tidligereVirksomhet?.OrganizationNumber.length) {
+            setTidligereVirksomhet(tidligereOrganisasjoner!![0])
+        }
+    }, []);
 
   return (
       <div className={'mine-ansatte__velg-tidligere-virksomhet'}>
--- src/mocking/mockresponsFraAltinn.tsx
+++ src/mocking/mockresponsFraAltinn.tsx
@@ -167,27 +167,11 @@ export const tilgangTilAtinntjenesteRespons: OrganisasjonlowerCase[] = [
 
 export const tidligerVirksomheter: OrganisasjonlowerCase[] = [
     {
-        name: 'MALMEFJORDEN OG RIDABU REGNSKAP',
-        type: 'Enterprise',
-        parentOrganizationNumber: null,
-        organizationNumber: '810825472',
+        name: 'HASLUM OG HOLTE REGNSKAP',
+        type: 'BEDR',
+        parentOrganizationNumber: '810825472',
+        organizationNumber: '954168395',
         organizationForm: 'AS',
-        status: 'Active'
-    },
-    {
-        name: 'MAURA OG KOLBU REGNSKAP',
-        type: 'Business',
-        parentOrganizationNumber: '810825472',
-        organizationNumber: '910825518',
-        organizationForm: 'BEDR',
-        status: 'Active'
-    },
-    {
-        name: 'GAMLE FREDRIKSTAD OG RAMNES REGNSKAP',
-        type: 'Business',
-        parentOrganizationNumber: '810825472',
-        organizationNumber: '910825526',
-        organizationForm: 'BEDR',
         status: 'Active'
     }
 ];
