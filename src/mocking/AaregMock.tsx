dd04c876ce54625234dbb2723e13198795c3b4e3
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-09-08 12:56:09 +0200

    velger forte bedrift i lista over gamle bedrifter

--- src/mocking/AaregMock.tsx
+++ src/mocking/AaregMock.tsx
@@ -1,6 +1,6 @@
-import fetchMock from 'fetch-mock';
+import fetchMock, {MockOptions, MockOptionsMethodGet} from 'fetch-mock';
 import {hentArbeidsforholdLinkNyBackend, hentTidligereVirksomheterLink} from '../App/lenker';
-import { AaregMockObjekt } from './funksjonerForAlageAAregMock';
+import {AaregMockObjekt, AaregMockObjektForNedlagtVirksomhet} from './funksjonerForAlageAAregMock';
 import {tidligerVirksomheter} from "./mockresponsFraAltinn";
 const delay = new Promise(res => setTimeout(res, 4000));
 fetchMock
@@ -10,6 +10,28 @@ fetchMock
           return AaregMockObjekt;
         })
     )
+    .spy();
+
+let headere = {"orgnr": "954168395", "jurenhet": "810825472"};
+
+/*fetchMock
+    .get(
+        hentArbeidsforholdLinkNyBackend(),AaregMockObjekt,{
+            delay: 1000, // fake a slow network
+            headers: {
+                orgnr: '910825526', jurenhet: "810825472"
+            }})
+    .spy();
+
+ */
+
+fetchMock
+    .get(
+        hentArbeidsforholdLinkNyBackend(),AaregMockObjektForNedlagtVirksomhet,{
+            delay: 1000, // fake a slow network
+            headers: {
+                orgnr: "954168395", jurenhet: "810825472"
+            }})
     .spy();
 
 fetchMock
