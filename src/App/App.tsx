06701ea80d2de392738752672411e3636ef7400a
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-09-22 13:09:10 +0200

    nytt forsok pa a skjule bedriftsmenyen

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -257,14 +257,23 @@ const App = () => {
         <div className="app">
             <LoginBoundary>
                 <Router basename={basename}>
-                    {organisasjonerFraAltinnLasteState !== APISTATUS.LASTER && (
+                    {organisasjonerFraAltinnLasteState !== APISTATUS.LASTER && ERPATIDLIGEREARBEIDSFORHOLD?
                         <HovedBanner
+                            setEndringIUrlAlert={setNåværendeUrlString}
+                            valgtAktivOrganisasjon={valgtAktivOrganisasjon}
+                            byttOrganisasjon={setValgtOrg}
+                            organisasjoner={undefined}
+                            sidetittel={'Tidligere arbeidsforhold'}
+                        />
+                        :
+                        <HovedBanner
+                            sidetittel={'Arbeidsforhold'}
                             setEndringIUrlAlert={setNåværendeUrlString}
                             valgtAktivOrganisasjon={valgtAktivOrganisasjon}
                             byttOrganisasjon={setValgtOrg}
                             organisasjoner={organisasjonerFraAltinnLasteState === APISTATUS.OK ? organisasjonerFraAltinn : []}
                         />
-                    )}
+                    }
                     {organisasjonerFraAltinnLasteState === APISTATUS.OK ? (
                         <>
                             {tilgangArbeidsforholdState !== TILGANGSSTATE.LASTER && (
