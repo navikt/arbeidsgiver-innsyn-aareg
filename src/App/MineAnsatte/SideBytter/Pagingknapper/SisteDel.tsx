83738ca1cf3d4a96f9aac70ca67ba270a6df46b8
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2020-11-10 17:11:18 +0100

    paginering med piltaster fungerer utenom at den gar out of bounds

--- src/App/MineAnsatte/SideBytter/Pagingknapper/SisteDel.tsx
+++ src/App/MineAnsatte/SideBytter/Pagingknapper/SisteDel.tsx
@@ -6,6 +6,7 @@ interface Props {
     setParameterIUrl: (parameter: string, variabel: string) => void;
     siderTilsammen: number;
     nåVærendeSidetall: number;
+    erØversteSidebytter: boolean
 }
 
 const SisteDel: FunctionComponent<Props> = props => {
@@ -16,6 +17,7 @@ const SisteDel: FunctionComponent<Props> = props => {
                 siderTilsammen={props.siderTilsammen}
                 nåVærendeSidetall={props.nåVærendeSidetall}
                 setParameterIUrl={props.setParameterIUrl}
+                erØversteSidebytter={props.erØversteSidebytter}
             />
             ...
             <PagineringsKnapp
@@ -23,18 +25,21 @@ const SisteDel: FunctionComponent<Props> = props => {
                 nåVærendeSidetall={props.nåVærendeSidetall}
                 sidetall={props.siderTilsammen - 2}
                 siderTilsammen={props.siderTilsammen}
+                erØversteSidebytter={props.erØversteSidebytter}
             />
             <PagineringsKnapp
                 setParameterIUrl={props.setParameterIUrl}
                 sidetall={props.siderTilsammen - 1}
                 siderTilsammen={props.siderTilsammen}
                 nåVærendeSidetall={props.nåVærendeSidetall}
+                erØversteSidebytter={props.erØversteSidebytter}
             />
             <PagineringsKnapp
                 setParameterIUrl={props.setParameterIUrl}
                 sidetall={props.siderTilsammen}
                 siderTilsammen={props.siderTilsammen}
                 nåVærendeSidetall={props.nåVærendeSidetall}
+                erØversteSidebytter={props.erØversteSidebytter}
             />
         </>
     );
