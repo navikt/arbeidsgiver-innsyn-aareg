1ecbd7bb0bc2e690de5bf07eb46f1cf29ea894bb
Ken Gullaksen <ken.gullaksen@nav.no>
2021-12-27 11:42:45 +0100

    forsøk på å bumpe cra
    her var det mye breakage.
    Har også tatt bort mye ubrukte ting fra package.json og bumpet det jeg kunne.
    
    yarn start kjører med det feiler i browser pga noe polyfill greier som feiler rundt process i readable-stream.
    denne er kanskje relevant https://sanchit3b.medium.com/how-to-polyfill-node-core-modules-in-webpack-5-905c1f5504a0

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,27 @@
 const CracoLessPlugin = require('craco-less');
 
 module.exports = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => ({
+            ...webpackConfig,
+            ...{
+                resolve: {
+                    ...webpackConfig.resolve,
+                    fallback: {
+                        "assert": require.resolve("assert/"),
+                        "buffer": require.resolve("buffer/"),
+                        "crypto": require.resolve("crypto-browserify"),
+                        "fs": require.resolve("browserify-fs"),
+                        "path": require.resolve("path-browserify"),
+                        "process": require.resolve("process/browser"),
+                        "stream": require.resolve("stream-browserify"),
+                        "util": require.resolve("util/"),
+                        "zlib":  require.resolve("browserify-zlib"),
+                    }
+                }
+            }
+        }),
+    },
     devServer: {
         proxy: {
             '/arbeidsforhold/api': {
@@ -8,13 +29,13 @@ module.exports = {
                 pathRewrite: { '^/arbeidsforhold/api': '/ditt-nav-arbeidsgiver-api/api' }
             }
         },
-        before: app => {
+        onBeforeSetupMiddleware: ({app}) => {
             app.get('/arbeidsforhold/redirect-til-login', (req, res) => {
                 const loginUrl =
                     'http://localhost:8080/ditt-nav-arbeidsgiver-api/local/selvbetjening-login?redirect=http://localhost:3000/arbeidsforhold';
                 res.redirect(loginUrl);
             });
-        }
+        },
     },
     plugins: [{ plugin: CracoLessPlugin }]
 };
--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { render } from '@testing-library/react';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import App from './App';
 
 it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App />, div);
-    ReactDOM.unmountComponentAtNode(div);
+    render(<App />);
 });
 
 expect.extend(toHaveNoViolations);
 
 test('should have no a11y violations', async () => {
-    render(<App />, document.body);
-    const results = await axe(document.body);
+    const {container} = render(<App />);
+    const results = await axe(container);
     expect(results).toHaveNoViolations();
 });
