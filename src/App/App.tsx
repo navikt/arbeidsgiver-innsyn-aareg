5a3ed98481f77d6c1ad6abc647d13170d3f58826
Bendik Ibenholt <bendik.segrov.ibenholt@nav.no>
2022-01-03 14:08:01 +0100

    bump notifikasjon widget, legg notifikasjonprovider utenfor browserro… (#554)
    * bump notifikasjon widget, legg notifikasjonprovider utenfor browserrouter

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -13,6 +13,8 @@ import './App.less';
 import { useLocation } from 'react-router';
 import { loggSidevisning } from '../utils/amplitudefunksjonerForLogging';
 import { LoginContext, LoginProvider } from './Context/LoginProvider';
+import { NotifikasjonWidgetProvider } from '@navikt/arbeidsgiver-notifikasjon-widget';
+import { gittMiljø } from '../utils/environment';
 
 const AmplitudeSidevisningEventLogger: FunctionComponent = props => {
     const location = useLocation();
@@ -22,46 +24,53 @@ const AmplitudeSidevisningEventLogger: FunctionComponent = props => {
     }, [location.pathname]);
     return <>{props.children}</>;
 };
-
+const miljø = gittMiljø<'local' | 'labs' | 'dev' | 'prod'>({
+    prod: 'prod',
+    dev: 'dev',
+    labs: 'labs',
+    other: 'local'
+});
 
 const App = () => {
     return (
         <div className='app'>
             <LoginProvider>
-                <BrowserRouter basename={basename}>
-                    <AmplitudeSidevisningEventLogger>
-                        <LoginBoundary>
-                            <FeatureToggleProvider>
-                                <AltinnorganisasjonerProvider>
-                                    <BedriftsmenyProvider>
-                                        <ArbeidsforholdProvider>
-                                            <FiltrerteOgSorterteArbeidsforholdProvider>
-                                                <Switch>
-                                                    <Route exact path='/' component={MineNåværendeArbeidsforhold} />
-                                                    <Route
-                                                        exact
-                                                        path='/enkeltArbeidsforhold'
-                                                        component={EnkeltArbeidsforhold}
-                                                    />
-                                                    <Route
-                                                        exact
-                                                        path='/tidligere-arbeidsforhold'
-                                                        component={MineTidligereArbeidsforhold}
-                                                    />
-                                                    <Route
-                                                        exact
-                                                        path='/tidligere-arbeidsforhold/enkeltArbeidsforhold'
-                                                        component={EnkeltArbeidsforhold}
-                                                    />
-                                                </Switch>
-                                            </FiltrerteOgSorterteArbeidsforholdProvider>
-                                        </ArbeidsforholdProvider>
-                                    </BedriftsmenyProvider>
-                                </AltinnorganisasjonerProvider>
-                            </FeatureToggleProvider>
-                        </LoginBoundary>
-                    </AmplitudeSidevisningEventLogger>
-                </BrowserRouter>
+                <NotifikasjonWidgetProvider miljo={miljø}>
+                    <BrowserRouter basename={basename}>
+                        <AmplitudeSidevisningEventLogger>
+                            <LoginBoundary>
+                                <FeatureToggleProvider>
+                                    <AltinnorganisasjonerProvider>
+                                        <BedriftsmenyProvider>
+                                            <ArbeidsforholdProvider>
+                                                <FiltrerteOgSorterteArbeidsforholdProvider>
+                                                    <Switch>
+                                                        <Route exact path='/' component={MineNåværendeArbeidsforhold} />
+                                                        <Route
+                                                            exact
+                                                            path='/enkeltArbeidsforhold'
+                                                            component={EnkeltArbeidsforhold}
+                                                        />
+                                                        <Route
+                                                            exact
+                                                            path='/tidligere-arbeidsforhold'
+                                                            component={MineTidligereArbeidsforhold}
+                                                        />
+                                                        <Route
+                                                            exact
+                                                            path='/tidligere-arbeidsforhold/enkeltArbeidsforhold'
+                                                            component={EnkeltArbeidsforhold}
+                                                        />
+                                                    </Switch>
+                                                </FiltrerteOgSorterteArbeidsforholdProvider>
+                                            </ArbeidsforholdProvider>
+                                        </BedriftsmenyProvider>
+                                    </AltinnorganisasjonerProvider>
+                                </FeatureToggleProvider>
+                            </LoginBoundary>
+                        </AmplitudeSidevisningEventLogger>
+                    </BrowserRouter>
+                </NotifikasjonWidgetProvider>
             </LoginProvider>
         </div>
     );
