62e79d9613126aba10cfea30a17ac02d353eecee
Ken Gullaksen <ken.gullaksen@nav.no>
2022-10-06 14:43:09 +0200

    flytt proxy ting ut av craco.config
    craco config brukes i ci bygg, og krever avhengigheter i deps i stedet for devdeps

---
+++ src/setupProxy.js
@@ -0,0 +1,33 @@
+/* Automatically picked up by react-script in development mode. */
+
+module.exports = function(app) {
+    const {applyNotifikasjonMockMiddleware} = require('@navikt/arbeidsgiver-notifikasjoner-brukerapi-mock');
+    const cookieParser = require('cookie-parser');
+
+    applyNotifikasjonMockMiddleware({app, path: '/arbeidsforhold/notifikasjon-bruker-api'});
+    app.use(cookieParser());
+    app.get('/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/innlogget', (req, res) => {
+        console.log("innlogget?")
+        const token = req.cookies.hasOwnProperty('selvbetjening-idtoken')
+        if (token) {
+            console.log("innlogget? ja (cookie selvbetjening-idtoken eksisterer)")
+            res.status(200).send()
+        } else {
+            console.log("innlogget? nei (cookie selvbetjening-idtoken mangler)")
+            res.status(401).send()
+        }
+    });
+    app.get('/arbeidsforhold/redirect-til-login', async (req, res) => {
+        const response = await fetch('https://fakedings.dev-gcp.nais.io/fake/custom', {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/x-www-form-urlencoded"
+            },
+            body: `sub=00112233445&aud=${encodeURIComponent("bruker-api")}&acr=Level4`
+        });
+        const token = await response.text()
+        res.cookie("selvbetjening-idtoken", token)
+        console.log(`login: setter selvbetjening-idtoken til ${token}`)
+        res.redirect("http://localhost:3000/arbeidsforhold/");
+    });
+};
