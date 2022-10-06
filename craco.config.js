8c2f44dff0d1122e4345d2dd3e68381cf30f2c20
Ken Gullaksen <ken.gullaksen@nav.no>
2022-10-06 14:43:09 +0200

    flytt proxy ting ut av craco.config
    craco config brukes i ci bygg, og krever avhengigheter i deps i stedet for devdeps

--- craco.config.js
+++ craco.config.js
@@ -1,6 +1,5 @@
 const CracoLessPlugin = require('craco-less');
 const {ProvidePlugin} = require("webpack");
-const {applyNotifikasjonMockMiddleware} = require('@navikt/arbeidsgiver-notifikasjoner-brukerapi-mock');
 
 module.exports = {
     webpack: {
@@ -30,40 +29,6 @@ module.exports = {
                 }
             }
         }),
-    },
-    devServer: {
-
-        setupMiddlewares: (middlewares, {app}) => {
-            const cookieParser = require('cookie-parser');
-            app.use(cookieParser());
-
-            applyNotifikasjonMockMiddleware({app, path: '/arbeidsforhold/notifikasjon-bruker-api'});
-            app.get('/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/innlogget', (req, res) => {
-                console.log("innlogget?")
-                const token = req.cookies.hasOwnProperty('selvbetjening-idtoken')
-                if (token) {
-                    console.log("innlogget? ja (cookie selvbetjening-idtoken eksisterer)")
-                    res.status(200).send()
-                } else {
-                    console.log("innlogget? nei (cookie selvbetjening-idtoken mangler)")
-                    res.status(401).send()
-                }
-            });
-            app.get('/arbeidsforhold/redirect-til-login', async (req, res) => {
-                const response = await fetch('https://fakedings.dev-gcp.nais.io/fake/custom', {
-                    method: 'POST',
-                    headers: {
-                        "Content-type": "application/x-www-form-urlencoded"
-                    },
-                    body: `sub=00112233445&aud=${encodeURIComponent("bruker-api")}&acr=Level4`
-                });
-                const token = await response.text()
-                res.cookie("selvbetjening-idtoken", token)
-                console.log(`login: setter selvbetjening-idtoken til ${token}`)
-                res.redirect("http://localhost:3000/arbeidsforhold/");
-            });
-            return middlewares;
-        },
     },
     plugins: [{ plugin: CracoLessPlugin }]
 };
