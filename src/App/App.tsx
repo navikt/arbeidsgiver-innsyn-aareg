4f80b18ada8960a7f7834805c0b563f96a94f96c
Knut Arild Slåtsve <53740375+KnutArildSlaatsve@users.noreply.github.com>
2023-09-08 08:35:51 +0200

    Fjernet bruk av unleashed (#724)

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,25 +1,24 @@
-import React, { FunctionComponent, useContext, useEffect } from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { basename } from './paths';
+import React, {FunctionComponent, useContext, useEffect} from 'react';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import {basename} from './paths';
 import LoginBoundary from './LoginBoundary';
-import { FeatureToggleProvider } from './Context/FeatureToggleProvider';
-import { ArbeidsforholdProvider } from './Context/ArbeidsforholdProvider';
-import { AltinnorganisasjonerProvider } from './Context/AltinnorganisasjonerProvider';
+import {ArbeidsforholdProvider} from './Context/ArbeidsforholdProvider';
+import {AltinnorganisasjonerProvider} from './Context/AltinnorganisasjonerProvider';
 import BedriftsmenyProvider from './Context/BedriftsmenyProvider';
 import FiltrerteOgSorterteArbeidsforholdProvider from './Context/FiltrerteOgSorterteArbeidsforholdProvider';
 import EnkeltArbeidsforhold from './MineAnsatte/EnkeltArbeidsforhold/EnkeltArbeidsforhold';
-import { MineNåværendeArbeidsforhold, MineTidligereArbeidsforhold } from './MineAnsatte/MineAnsatte';
+import {MineNåværendeArbeidsforhold, MineTidligereArbeidsforhold} from './MineAnsatte/MineAnsatte';
 import './App.less';
-import { useLocation } from 'react-router';
-import { loggSidevisning } from '../utils/amplitudefunksjonerForLogging';
-import { LoginContext, LoginProvider } from './Context/LoginProvider';
-import { NotifikasjonWidgetProvider } from '@navikt/arbeidsgiver-notifikasjon-widget';
-import { gittMiljø } from '../utils/environment';
+import {useLocation} from 'react-router';
+import {loggSidevisning} from '../utils/amplitudefunksjonerForLogging';
+import {LoginContext, LoginProvider} from './Context/LoginProvider';
+import {NotifikasjonWidgetProvider} from '@navikt/arbeidsgiver-notifikasjon-widget';
+import {gittMiljø} from '../utils/environment';
 import "@navikt/ds-css"
 
 const AmplitudeSidevisningEventLogger: FunctionComponent = props => {
     const location = useLocation();
-    const { innlogget } = useContext(LoginContext);
+    const {innlogget} = useContext(LoginContext);
     useEffect(() => {
         loggSidevisning(location.pathname, innlogget);
     }, [location.pathname]);
@@ -40,33 +39,31 @@ const App = () => {
                     <BrowserRouter basename={basename}>
                         <AmplitudeSidevisningEventLogger>
                             <LoginBoundary>
-                                <FeatureToggleProvider>
-                                    <AltinnorganisasjonerProvider>
-                                        <BedriftsmenyProvider>
-                                            <ArbeidsforholdProvider>
-                                                <FiltrerteOgSorterteArbeidsforholdProvider>
-                                                    <Routes>
-                                                        <Route
-                                                            path='/'
-                                                            element={<MineNåværendeArbeidsforhold/>} />
-                                                        <Route
-                                                            path='/enkeltArbeidsforhold'
-                                                            element={<EnkeltArbeidsforhold/>}
-                                                        />
-                                                        <Route
-                                                            path='/tidligere-arbeidsforhold'
-                                                            element={<MineTidligereArbeidsforhold/>}
-                                                        />
-                                                        <Route
-                                                            path='/tidligere-arbeidsforhold/enkeltArbeidsforhold'
-                                                            element={<EnkeltArbeidsforhold/>}
-                                                        />
-                                                    </Routes>
-                                                </FiltrerteOgSorterteArbeidsforholdProvider>
-                                            </ArbeidsforholdProvider>
-                                        </BedriftsmenyProvider>
-                                    </AltinnorganisasjonerProvider>
-                                </FeatureToggleProvider>
+                                <AltinnorganisasjonerProvider>
+                                    <BedriftsmenyProvider>
+                                        <ArbeidsforholdProvider>
+                                            <FiltrerteOgSorterteArbeidsforholdProvider>
+                                                <Routes>
+                                                    <Route
+                                                        path='/'
+                                                        element={<MineNåværendeArbeidsforhold/>}/>
+                                                    <Route
+                                                        path='/enkeltArbeidsforhold'
+                                                        element={<EnkeltArbeidsforhold/>}
+                                                    />
+                                                    <Route
+                                                        path='/tidligere-arbeidsforhold'
+                                                        element={<MineTidligereArbeidsforhold/>}
+                                                    />
+                                                    <Route
+                                                        path='/tidligere-arbeidsforhold/enkeltArbeidsforhold'
+                                                        element={<EnkeltArbeidsforhold/>}
+                                                    />
+                                                </Routes>
+                                            </FiltrerteOgSorterteArbeidsforholdProvider>
+                                        </ArbeidsforholdProvider>
+                                    </BedriftsmenyProvider>
+                                </AltinnorganisasjonerProvider>
                             </LoginBoundary>
                         </AmplitudeSidevisningEventLogger>
                     </BrowserRouter>
