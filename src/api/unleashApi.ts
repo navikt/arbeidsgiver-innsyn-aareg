8bd4dd1b01066c698a43bec4e44cfd5127f140c6
Knut Arild Slåtsve <53740375+KnutArildSlaatsve@users.noreply.github.com>
2023-09-08 08:35:51 +0200

    Fjernet bruk av unleashed (#724)

--- src/api/unleashApi.ts
+++
@@ -1,7 +0,0 @@
-import {hentFeatureTogglesLenke} from "../App/lenker";
-import {FeatureToggles} from "../App/Context/FeatureToggleProvider";
-
-export async function hentFeatureToggles (): Promise<FeatureToggles> {
-    const response = await fetch(hentFeatureTogglesLenke(), { credentials: 'same-origin' });
-    return await response.json();
-};
