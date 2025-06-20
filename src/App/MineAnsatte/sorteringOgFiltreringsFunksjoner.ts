04674aede92179cc049f6292bf7de4923448297b
Ken Gullaksen <ken.gullaksen@nav.no>
2025-06-20 14:39:24 +0200

    cleanup and tweak pagination

--- src/App/MineAnsatte/sorteringOgFiltreringsFunksjoner.ts
+++ src/App/MineAnsatte/sorteringOgFiltreringsFunksjoner.ts
@@ -1,13 +1,24 @@
-import { SorteringsAttributt } from './MineAnsatte';
 import { Arbeidsforhold } from '../Objekter/ArbeidsForhold';
-import { byggArbeidsforholdSokeresultat } from './Sokefelt/byggArbeidsforholdSokeresultat';
-import { useSearchParameters } from '../../utils/UrlManipulation';
 
 export const StatusFilterValues = ['Alle', 'Aktive', 'Avsluttede'] as const;
 export type StatusFilter = (typeof StatusFilterValues)[number];
-
 export function mapToStatusFilter(value: string | null): StatusFilter | null {
     return StatusFilterValues.includes(value as StatusFilter) ? (value as StatusFilter) : null;
+}
+
+export const SorteringValues = [
+    'NAVN',
+    'FNR',
+    'YRKE',
+    'STARTDATO',
+    'SLUTTDATO',
+    'VARSEL',
+    'PERMITTERINGSPROSENT',
+    'STILLINGSPROSENT',
+] as const;
+export type Sortering = (typeof SorteringValues)[number];
+export function mapToSortering(value: string | null): Sortering | null {
+    return SorteringValues.includes(value as Sortering) ? (value as Sortering) : null;
 }
 
 export type ArbeidsForholdGroupedByFilterStatus = Record<StatusFilter, Arbeidsforhold[]>;
@@ -41,119 +52,81 @@ export const arbeidsForholdGroupedByFilterStatus = (
     );
 };
 
-export const sorterBasertPaDatoFom = (arbeidsforhold: Array<Arbeidsforhold>) => {
-    const sortert: Arbeidsforhold[] = arbeidsforhold.sort((a, b) => {
-        const datoA = new Date(a.ansattFom);
-        const datoB = new Date(b.ansattFom);
-        if (datoA > datoB) {
-            return -1;
-        }
-        return 1;
-    });
-    return sortert;
-};
-
-const sorterBasertPaDatoTom = (arbeidsforhold: Arbeidsforhold[]) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (!a.ansattTom) {
-            return -1;
-        }
-        if (!b.ansattTom) {
-            return 1;
-        }
-        const datoA = new Date(a.ansattTom);
-        const datoB = new Date(b.ansattTom);
-        if (datoA < datoB) {
-            return -1;
-        }
-        return 1;
-    });
-};
-
-const sorterBasertPaNavn = (arbeidsforhold: Arbeidsforhold[]) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (a.arbeidstaker.navn > b.arbeidstaker.navn) {
-            return 1;
-        }
-        return -1;
-    });
-};
-
-const sorterBasertPaProsent = (
-    arbeidsforhold: Arbeidsforhold[],
-    sorterPaStillingsprosent: boolean,
-    sorterPaPermisjonsprosent: boolean
-) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (sorterPaStillingsprosent) {
-            if (Number(a.stillingsprosent) > Number(b.stillingsprosent)) {
-                return 1;
-            } else return -1;
-        }
-        if (sorterPaPermisjonsprosent) {
-            if (Number(a.permisjonPermitteringsprosent) > Number(b.permisjonPermitteringsprosent)) {
-                return 1;
-            } else return -1;
-        } else {
-            return 1;
-        }
-    });
-};
-
-const sorterBasertPaKode = (arbeidsforhold: Arbeidsforhold[]) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (!a.varsler) {
-            return -1;
-        }
-        if (!b.varsler) {
-            return 1;
-        }
-        if (a.varsler.length > b.varsler.length) {
-            return 1;
-        }
-        return -1;
-    });
-};
-
-const sorterBasertPaFnr = (arbeidsforhold: Arbeidsforhold[]) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (a.arbeidstaker.offentligIdent > b.arbeidstaker.offentligIdent) {
-            return 1;
-        }
-        return -1;
-    });
-};
-
-const sorterBasertPaYrke = (arbeidsforhold: Arbeidsforhold[]) => {
-    return arbeidsforhold.sort((a, b) => {
-        if (a.yrkesbeskrivelse > b.yrkesbeskrivelse) {
-            return 1;
-        }
-        return -1;
-    });
-};
-
-export const sorterArbeidsforhold = (
-    arbeidsforhold: Arbeidsforhold[],
-    atributt: SorteringsAttributt
-) => {
+export const sorterArbeidsforhold = (arbeidsforhold: Arbeidsforhold[], atributt: Sortering) => {
     switch (atributt) {
-        case SorteringsAttributt.NAVN:
-            return sorterBasertPaNavn(arbeidsforhold);
-        case SorteringsAttributt.STARTDATO:
-            return sorterBasertPaDatoFom(arbeidsforhold);
-        case SorteringsAttributt.SLUTTDATO:
-            return sorterBasertPaDatoTom(arbeidsforhold);
-        case SorteringsAttributt.FNR:
-            return sorterBasertPaFnr(arbeidsforhold);
-        case SorteringsAttributt.YRKE:
-            return sorterBasertPaYrke(arbeidsforhold);
-        case SorteringsAttributt.VARSEL:
-            return sorterBasertPaKode(arbeidsforhold);
-        case SorteringsAttributt.PERMITTERINGSPROSENT:
-            return sorterBasertPaProsent(arbeidsforhold, false, true);
-        case SorteringsAttributt.STILLINGSPROSENT:
-            return sorterBasertPaProsent(arbeidsforhold, true, false);
+        case 'NAVN':
+            return arbeidsforhold.sort((a, b) => {
+                if (a.arbeidstaker.navn > b.arbeidstaker.navn) {
+                    return 1;
+                }
+                return -1;
+            });
+        case 'STARTDATO':
+            return arbeidsforhold.sort((a, b) => {
+                const datoA = new Date(a.ansattFom);
+                const datoB = new Date(b.ansattFom);
+                if (datoA > datoB) {
+                    return -1;
+                }
+                return 1;
+            });
+        case 'SLUTTDATO':
+            return arbeidsforhold.sort((a, b) => {
+                if (!a.ansattTom) {
+                    return -1;
+                }
+                if (!b.ansattTom) {
+                    return 1;
+                }
+                const datoA = new Date(a.ansattTom);
+                const datoB = new Date(b.ansattTom);
+                if (datoA < datoB) {
+                    return -1;
+                }
+                return 1;
+            });
+        case 'FNR':
+            return arbeidsforhold.sort((a, b) => {
+                if (a.arbeidstaker.offentligIdent > b.arbeidstaker.offentligIdent) {
+                    return 1;
+                }
+                return -1;
+            });
+        case 'YRKE':
+            return arbeidsforhold.sort((a, b) => {
+                if (a.yrkesbeskrivelse > b.yrkesbeskrivelse) {
+                    return 1;
+                }
+                return -1;
+            });
+        case 'VARSEL':
+            return arbeidsforhold.sort((a, b) => {
+                if (!a.varsler) {
+                    return -1;
+                }
+                if (!b.varsler) {
+                    return 1;
+                }
+                if (a.varsler.length > b.varsler.length) {
+                    return 1;
+                }
+                return -1;
+            });
+        case 'PERMITTERINGSPROSENT':
+            return arbeidsforhold.sort((a, b) => {
+                if (
+                    Number(a.permisjonPermitteringsprosent) >
+                    Number(b.permisjonPermitteringsprosent)
+                ) {
+                    return 1;
+                } else return -1;
+            });
+        case 'STILLINGSPROSENT':
+            return arbeidsforhold.sort((a, b) => {
+                if (Number(a.stillingsprosent) > Number(b.stillingsprosent)) {
+                    return 1;
+                } else return -1;
+            });
         default:
             return arbeidsforhold;
     }
