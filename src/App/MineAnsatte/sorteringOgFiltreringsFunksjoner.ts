2f744394704396a13f6ace08cb6a156485a3cd42
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-06-24 13:55:53 +0200

    Bruker Table fra aksel komponent og forenkler sortering

--- src/App/MineAnsatte/sorteringOgFiltreringsFunksjoner.ts
+++ src/App/MineAnsatte/sorteringOgFiltreringsFunksjoner.ts
@@ -2,6 +2,7 @@ import { Arbeidsforhold } from '../Objekter/ArbeidsForhold';
 
 export const StatusFilterValues = ['Alle', 'Aktive', 'Avsluttede'] as const;
 export type StatusFilter = (typeof StatusFilterValues)[number];
+
 export function mapToStatusFilter(value: string | null): StatusFilter | null {
     return StatusFilterValues.includes(value as StatusFilter) ? (value as StatusFilter) : null;
 }
@@ -16,7 +17,9 @@ export const SorteringValues = [
     'PERMITTERINGSPROSENT',
     'STILLINGSPROSENT',
 ] as const;
+
 export type Sortering = (typeof SorteringValues)[number];
+
 export function mapToSortering(value: string | null): Sortering | null {
     return SorteringValues.includes(value as Sortering) ? (value as Sortering) : null;
 }
@@ -52,82 +55,47 @@ export const arbeidsForholdGroupedByFilterStatus = (
     );
 };
 
-export const sorterArbeidsforhold = (arbeidsforhold: Arbeidsforhold[], atributt: Sortering) => {
-    switch (atributt) {
-        case 'NAVN':
-            return arbeidsforhold.sort((a, b) => {
-                if (a.arbeidstaker.navn > b.arbeidstaker.navn) {
-                    return 1;
-                }
-                return -1;
-            });
-        case 'STARTDATO':
-            return arbeidsforhold.sort((a, b) => {
-                const datoA = new Date(a.ansattFom);
-                const datoB = new Date(b.ansattFom);
-                if (datoA > datoB) {
-                    return -1;
-                }
-                return 1;
-            });
-        case 'SLUTTDATO':
-            return arbeidsforhold.sort((a, b) => {
-                if (!a.ansattTom) {
-                    return -1;
-                }
-                if (!b.ansattTom) {
-                    return 1;
-                }
-                const datoA = new Date(a.ansattTom);
-                const datoB = new Date(b.ansattTom);
-                if (datoA < datoB) {
-                    return -1;
-                }
-                return 1;
-            });
-        case 'FNR':
-            return arbeidsforhold.sort((a, b) => {
-                if (a.arbeidstaker.offentligIdent > b.arbeidstaker.offentligIdent) {
-                    return 1;
-                }
-                return -1;
-            });
-        case 'YRKE':
-            return arbeidsforhold.sort((a, b) => {
-                if (a.yrkesbeskrivelse > b.yrkesbeskrivelse) {
-                    return 1;
-                }
-                return -1;
-            });
-        case 'VARSEL':
-            return arbeidsforhold.sort((a, b) => {
-                if (!a.varsler) {
-                    return -1;
-                }
-                if (!b.varsler) {
-                    return 1;
-                }
-                if (a.varsler.length > b.varsler.length) {
-                    return 1;
-                }
-                return -1;
-            });
-        case 'PERMITTERINGSPROSENT':
-            return arbeidsforhold.sort((a, b) => {
-                if (
-                    Number(a.permisjonPermitteringsprosent) >
-                    Number(b.permisjonPermitteringsprosent)
-                ) {
-                    return 1;
-                } else return -1;
-            });
-        case 'STILLINGSPROSENT':
-            return arbeidsforhold.sort((a, b) => {
-                if (Number(a.stillingsprosent) > Number(b.stillingsprosent)) {
-                    return 1;
-                } else return -1;
-            });
-        default:
-            return arbeidsforhold;
-    }
+export const sorterArbeidsforhold = (
+    arbeidsforhold: Arbeidsforhold[],
+    attributt: Sortering,
+    direction: 'ascending' | 'descending' | 'none'
+): Arbeidsforhold[] => {
+    const sorted = [...arbeidsforhold];
+    sorted.sort((a, b) => {
+        const reverse = direction === 'descending' ? -1 : 1;
+
+        switch (attributt) {
+            case 'NAVN':
+                return a.arbeidstaker.navn.localeCompare(b.arbeidstaker.navn) * reverse;
+            case 'STARTDATO':
+                return (
+                    (new Date(a.ansattFom).getTime() - new Date(b.ansattFom).getTime()) * reverse
+                );
+            case 'SLUTTDATO':
+                return (
+                    (new Date(a.ansattTom ?? 0).getTime() - new Date(b.ansattTom ?? 0).getTime()) *
+                    reverse
+                );
+            case 'FNR':
+                return (
+                    a.arbeidstaker.offentligIdent.localeCompare(b.arbeidstaker.offentligIdent) *
+                    reverse
+                );
+            case 'YRKE':
+                return a.yrkesbeskrivelse.localeCompare(b.yrkesbeskrivelse) * reverse;
+            case 'VARSEL':
+                return ((a.varsler?.length ?? 0) - (b.varsler?.length ?? 0)) * reverse;
+            case 'PERMITTERINGSPROSENT':
+                return (
+                    (Number(a.permisjonPermitteringsprosent) -
+                        Number(b.permisjonPermitteringsprosent)) *
+                    reverse
+                );
+            case 'STILLINGSPROSENT':
+                return (Number(a.stillingsprosent) - Number(b.stillingsprosent)) * reverse;
+            default:
+                return 0;
+        }
+    });
+    return sorted;
 };
