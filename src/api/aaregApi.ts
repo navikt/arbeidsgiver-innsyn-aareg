3fd82e5b8a1a24554f534843a10292ac3e631025
Ken Gullaksen <ken.gullaksen@nav.no>
2025-07-01 10:58:21 +0200

    fjern abortcontrollers

--- src/api/aaregApi.ts
+++ src/api/aaregApi.ts
@@ -1,27 +1,26 @@
 import {
     hentAntallArbeidsforholdLink,
-    hentArbeidsforholdLink, hentTidligereArbeidsforholdLink,
-    hentTidligereVirksomheterLink
+    hentArbeidsforholdLink,
+    hentTidligereArbeidsforholdLink,
+    hentTidligereVirksomheterLink,
 } from '../App/lenker';
 import { ObjektFraAAregisteret } from '../App/Objekter/ObjektFraAAreg';
 import { FetchError } from './api-utils';
 import { overSiktPerUnderenhetPar } from '../App/Objekter/OversiktOverAntallForholdPerUnderenhet';
-import {
-    loggArbeidsforholdLastet
-} from '../utils/amplitudefunksjonerForLogging';
+import { loggArbeidsforholdLastet } from '../utils/amplitudefunksjonerForLogging';
 import { Organisasjon } from '../App/Objekter/OrganisasjonFraAltinn';
 import { mapOrganisasjonerFraLowerCaseTilupper } from './altinnApi';
 
 export async function hentArbeidsforholdFraAAreg(
     underenhet: string,
     enhet: string,
-    signal: any,
     erTidligereArbeidsforhold: boolean
 ): Promise<ObjektFraAAregisteret> {
     const headere = lagHeadere(enhet, underenhet);
-    const linkTilEndepunkt = erTidligereArbeidsforhold ?
-        hentTidligereArbeidsforholdLink() : hentArbeidsforholdLink();
-    let response: Response = await fetch(linkTilEndepunkt, { headers: headere, signal: signal });
+    const linkTilEndepunkt = erTidligereArbeidsforhold
+        ? hentTidligereArbeidsforholdLink()
+        : hentArbeidsforholdLink();
+    let response: Response = await fetch(linkTilEndepunkt, { headers: headere });
     if (response.ok) {
         const jsonRespons: ObjektFraAAregisteret = await response.json();
         loggArbeidsforholdLastet(jsonRespons.arbeidsforholdoversikter);
@@ -33,11 +32,10 @@ export async function hentArbeidsforholdFraAAreg(
 
 export async function hentAntallArbeidsforholdFraAareg(
     underenhet: string,
-    enhet: string,
-    signal: any
+    enhet: string
 ): Promise<number | undefined> {
     const headere = lagHeadere(enhet, underenhet);
-    let respons = await fetch(hentAntallArbeidsforholdLink(), { headers: headere, signal: signal });
+    let respons = await fetch(hentAntallArbeidsforholdLink(), { headers: headere });
     if (respons.ok) {
         const jsonRespons: overSiktPerUnderenhetPar = await respons.json();
         return jsonRespons.second;
@@ -46,12 +44,9 @@ export async function hentAntallArbeidsforholdFraAareg(
     }
 }
 
-export async function hentTidligereVirksomheter(
-    enhet: string,
-    signal: any
-): Promise<Organisasjon[]> {
+export async function hentTidligereVirksomheter(enhet: string): Promise<Organisasjon[]> {
     const headere = lagHeadere(enhet);
-    let response: Response = await fetch(hentTidligereVirksomheterLink, { headers: headere, signal: signal });
+    let response: Response = await fetch(hentTidligereVirksomheterLink, { headers: headere });
     if (response.ok) {
         const organisasjoner = await response.json();
         return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
