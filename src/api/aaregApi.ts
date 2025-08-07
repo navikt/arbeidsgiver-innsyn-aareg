a63d2a7d9c37ea6e3a69a8b7569b1a65f78c55d5
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-08-07 12:53:58 +0200

    Forenkler fetching og bedriftsmeny

--- src/api/aaregApi.ts
+++ src/api/aaregApi.ts
@@ -1,8 +1,8 @@
 import { ObjektFraAAregisteret } from '../App/Objekter/ObjektFraAAreg';
-import { FetchError } from './api-utils';
+import { FetchError, fetchJson } from './api-utils';
 import { overSiktPerUnderenhetPar } from '../App/Objekter/OversiktOverAntallForholdPerUnderenhet';
 import { loggArbeidsforholdLastet } from '../utils/amplitudefunksjonerForLogging';
-import { Organisasjon } from '../App/Objekter/OrganisasjonFraAltinn';
+import { Organisasjon, OrganisasjonlowerCase } from '../App/Objekter/OrganisasjonFraAltinn';
 import { mapOrganisasjonerFraLowerCaseTilupper } from './altinnApi';
 
 export async function hentArbeidsforholdFraAAreg(
@@ -10,60 +10,46 @@ export async function hentArbeidsforholdFraAAreg(
     enhet: string,
     erTidligereArbeidsforhold: boolean
 ): Promise<ObjektFraAAregisteret> {
-    const response: Response = await fetch(
-        erTidligereArbeidsforhold
-            ? '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/tidligere-arbeidsforhold'
-            : '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/arbeidsforhold',
-        {
-            headers: {
-                jurenhet: enhet,
-                orgnr: underenhet,
-            },
-        }
-    );
-    if (response.ok) {
-        const jsonRespons: ObjektFraAAregisteret = await response.json();
-        loggArbeidsforholdLastet(jsonRespons.arbeidsforholdoversikter);
-        return jsonRespons;
-    } else {
-        throw new FetchError(response.statusText ?? response.type, response);
-    }
+    const url = erTidligereArbeidsforhold
+        ? '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/tidligere-arbeidsforhold'
+        : '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/arbeidsforhold';
+
+    const data = await fetchJson<ObjektFraAAregisteret>(url, {
+        jurenhet: enhet,
+        orgnr: underenhet,
+    });
+
+    loggArbeidsforholdLastet(data.arbeidsforholdoversikter);
+    return data;
 }
 
 export async function hentAntallArbeidsforholdFraAareg(
     underenhet: string,
     enhet: string
 ): Promise<number | undefined> {
-    const respons = await fetch(
-        '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/antall-arbeidsforhold',
-        {
-            headers: {
+    try {
+        const data = await fetchJson<overSiktPerUnderenhetPar>(
+            '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/antall-arbeidsforhold',
+            {
                 jurenhet: enhet,
                 orgnr: underenhet,
-            },
-        }
-    );
-    if (respons.ok) {
-        const jsonRespons: overSiktPerUnderenhetPar = await respons.json();
-        return jsonRespons.second;
-    } else {
+            }
+        );
+        return data.second;
+    } catch {
         return undefined;
     }
 }
 
-export async function hentTidligereVirksomheter(enhet: string): Promise<Organisasjon[]> {
-    const response: Response = await fetch(
+export async function hentTidligereVirksomheter(
+    enhet: string
+): Promise<Organisasjon[]> {
+    const data = await fetchJson<OrganisasjonlowerCase[]>(
         '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/tidligere-virksomheter',
         {
-            headers: {
-                jurenhet: enhet,
-            },
+            jurenhet: enhet,
         }
     );
-    if (response.ok) {
-        const organisasjoner = await response.json();
-        return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
-    } else {
-        throw new FetchError(response.statusText ?? response.type, response);
-    }
+
+    return mapOrganisasjonerFraLowerCaseTilupper(data);
 }
