296558c73cab36cc6af50efc95eab998f1c4f982
Jonas Enge <jonas.maccyber.enge@nav.no>
2025-08-07 12:53:58 +0200

    Forenkler fetching og bedriftsmeny

--- src/api/altinnApi.ts
+++ src/api/altinnApi.ts
@@ -1,4 +1,4 @@
-import { FetchError } from './api-utils';
+import { fetchJson } from './api-utils';
 import {
     Organisasjon,
     OrganisasjonlowerCase,
@@ -6,47 +6,37 @@ import {
 } from '../App/Objekter/OrganisasjonFraAltinn';
 
 export async function sjekkInnlogget(): Promise<boolean> {
-    let respons = await fetch('/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/innlogget');
+    const respons = await fetch('/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/innlogget');
     return respons.ok;
 }
 
 export async function hentOrganisasjonerFraAltinn(): Promise<Organisasjon[]> {
-    let respons = await fetch('/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/organisasjoner');
-    if (respons.ok) {
-        const organisasjoner = await respons.json();
-        return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
-    } else {
-        throw new FetchError(respons.statusText ?? respons.type, respons);
-    }
+    const organisasjoner = await fetchJson<OrganisasjonlowerCase[]>(
+        '/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/organisasjoner'
+    );
+    return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
 }
 
 export async function hentOrganisasjonerMedTilgangTilAltinntjeneste(
     serviceKode: string,
     serviceEdition: string
 ): Promise<Organisasjon[]> {
-    let respons = await fetch(
+    const organisasjoner = await fetchJson<OrganisasjonlowerCase[]>(
         `/arbeidsforhold/arbeidsgiver-arbeidsforhold/api/rettigheter-til-tjeneste?serviceKode=${serviceKode}&serviceEdition=${serviceEdition}`
     );
-    if (respons.ok) {
-        const organisasjoner = await respons.json();
-        return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
-    } else {
-        throw new FetchError(respons.statusText ?? respons.type, respons);
-    }
+    return mapOrganisasjonerFraLowerCaseTilupper(organisasjoner);
 }
 
 export const mapOrganisasjonerFraLowerCaseTilupper = (
     organisasjonerRaw: OrganisasjonlowerCase[]
 ): Organisasjon[] => {
-    return organisasjonerRaw.map((rawOrg) => {
-        return {
-            ...tomaAltinnOrganisasjon,
-            OrganizationNumber: rawOrg.organizationNumber,
-            OrganizationForm: rawOrg.organizationForm,
-            Name: rawOrg.name,
-            ParentOrganizationNumber: rawOrg.parentOrganizationNumber,
-            Type: rawOrg.type,
-            Status: rawOrg.status,
-        };
-    });
+    return organisasjonerRaw.map((rawOrg) => ({
+        ...tomaAltinnOrganisasjon,
+        OrganizationNumber: rawOrg.organizationNumber,
+        OrganizationForm: rawOrg.organizationForm,
+        Name: rawOrg.name,
+        ParentOrganizationNumber: rawOrg.parentOrganizationNumber,
+        Type: rawOrg.type,
+        Status: rawOrg.status,
+    }));
 };
